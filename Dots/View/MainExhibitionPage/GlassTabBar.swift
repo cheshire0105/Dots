da752aead6ec7a71393e557fdec40a7bc0992505
cheshire0105 <cheshire0105@gmail.com>
2023-10-23 13:23:46 +0900

    [feat] : 이미지 간격 조절 까지

--- Dots/View/MainExhibitionPage/GlassTabBar.swift
+++ Dots/View/MainExhibitionPage/GlassTabBar.swift
@@ -14,20 +14,6 @@ class GlassTabBar: UITabBar {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        let buttons = self.subviews.filter { $0 is UIControl } as! [UIControl]
-
-        if buttons.count == 5 {
-            let spacing: CGFloat = -20.0 // 이 값을 조절하여 탭바 아이템 간의 간격을 조절합니다.
-            let totalButtonWidth = self.frame.width - (spacing * CGFloat(buttons.count - 1))
-            let buttonWidth = totalButtonWidth / CGFloat(buttons.count)
-
-            var x: CGFloat = 0
-            for button in buttons {
-                button.frame = CGRect(x: x, y: button.frame.origin.y, width: buttonWidth, height: button.frame.height)
-                x += buttonWidth + spacing
-            }
-        }
-
         setupGlassEffect()
     }
 
@@ -74,40 +60,30 @@ class MainTabBarController: UITabBarController {
     // MARK: - TabBar Item Setup
     func setupTabBarItems() {
 
-        let imageYOffset: CGFloat = -2.0
-        let titleYOffset: CGFloat = 2.0
-
         let firstVC = MainExhibitionPage()
         firstVC.view.backgroundColor = .red
         firstVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "home"), tag: 0)
-        firstVC.tabBarItem.imageInsets = UIEdgeInsets(top: imageYOffset, left: 0, bottom: -imageYOffset, right: 0)
-        firstVC.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: titleYOffset)
+        firstVC.tabBarItem.imageInsets = UIEdgeInsets(top: -10, left: 0, bottom: 10, right: -50)
 
         // 동일한 방식으로 다른 뷰 컨트롤러의 탭바 아이템에도 적용
         let secondVC = SearchPage()
         secondVC.view.backgroundColor = .green
         secondVC.tabBarItem = UITabBarItem(title: "검색", image: UIImage(named: "search"), tag: 1)
-        secondVC.tabBarItem.imageInsets = UIEdgeInsets(top: imageYOffset, left: 0, bottom: -imageYOffset, right: 0)
-        secondVC.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: titleYOffset)
+        secondVC.tabBarItem.imageInsets = UIEdgeInsets(top: -10, left: 0, bottom: 10, right: -30)
 
         let thirdVC = PopularReviewsPage()
-        thirdVC.view.backgroundColor = .blue
         thirdVC.tabBarItem = UITabBarItem(title: "인기", image: UIImage(named: "hot"), tag: 2)
-        thirdVC.tabBarItem.imageInsets = UIEdgeInsets(top: imageYOffset, left: 0, bottom: -imageYOffset, right: 0)
-        thirdVC.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: titleYOffset)
+        thirdVC.tabBarItem.imageInsets = UIEdgeInsets(top: -10, left: 0, bottom: 10, right: 0)
 
         let fourthVC = MapPage()
         fourthVC.view.backgroundColor = .yellow
         fourthVC.tabBarItem = UITabBarItem(title: "지도", image: UIImage(named: "map"), tag: 3)
-        fourthVC.tabBarItem.imageInsets = UIEdgeInsets(top: imageYOffset, left: 0, bottom: -imageYOffset, right: 0)
-        fourthVC.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: titleYOffset)
+        fourthVC.tabBarItem.imageInsets = UIEdgeInsets(top: -10, left: -30, bottom: 10, right: 0)
 
         let fifthVC = Mypage()
         fifthVC.view.backgroundColor = .purple
         fifthVC.tabBarItem = UITabBarItem(title: "마이", image: UIImage(named: "mypage"), tag: 4)
-        fifthVC.tabBarItem.imageInsets = UIEdgeInsets(top: imageYOffset, left: 0, bottom: -imageYOffset, right: 0)
-        fifthVC.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: titleYOffset)
-
+        fifthVC.tabBarItem.imageInsets = UIEdgeInsets(top: -10, left: -50, bottom: 10, right: 0)
 
         self.viewControllers = [firstVC, secondVC, thirdVC, fourthVC, fifthVC]
     }
