2a4ab8524b18dd41b37b11bf351d43e1df1c2c85
cheshire0105 <cheshire0105@gmail.com>
2023-10-25 12:40:48 +0900

    [feat] : 네이버 지도 야간 모드로 화면 전체에 추가

--- Dots/View/MapPage/MapPage.swift
+++ Dots/View/MapPage/MapPage.swift
@@ -6,15 +6,21 @@
 //
 
 import UIKit
+import NMapsMap
 
 class MapPage: UIViewController {
+
+    var mapView: NMFMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 지도 뷰를 생성합니다.
+        mapView = NMFMapView(frame: self.view.frame)
+        mapView.mapType = .navi
+        mapView.isNightModeEnabled = true
+
+        // 지도 뷰를 메인 뷰에 추가합니다.
+        self.view.addSubview(mapView)
     }
-    
-
-
-
 }
