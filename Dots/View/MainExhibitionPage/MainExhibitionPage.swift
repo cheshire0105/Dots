3bd1355efb8d9460f338961aa49fe8a7fd670198
cheshire0105 <cheshire0105@gmail.com>
2023-10-25 10:04:36 +0900

    [feat] : 메인 전시 페이지에서 메인 컬렉션 뷰 밑에 추천 전시 컬렉션 뷰 추가 - 아직 코드 정리 이전

--- Dots/View/MainExhibitionPage/MainExhibitionPage.swift
+++ Dots/View/MainExhibitionPage/MainExhibitionPage.swift
@@ -9,6 +9,23 @@ import UIKit
 import RxSwift
 import RxCocoa
 import SnapKit
+import RxDataSources
+
+// 1. Section Model을 정의합니다.
+struct SectionItem {
+    var header: String
+    var items: [String]
+}
+
+extension SectionItem: SectionModelType {
+    typealias Item = String
+
+    init(original: SectionItem, items: [Item]) {
+        self = original
+        self.items = items
+    }
+}
+
 
 class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
 
@@ -34,12 +51,13 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
     // 새로운 컬렉션뷰를 정의합니다.
     // MainExhibitionCollectionView 초기화 부분에서 레이아웃 설정 변경
     lazy var MainExhibitionCollectionView: UICollectionView = {
-        let compositionalLayout = createCompositionalLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: compositionalLayout)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .black
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.register(MainExhibitionCollectionCell.self, forCellWithReuseIdentifier: "MainExhibitionCollectionCell")
-        //        collectionView.register(GraySquareCell.self, forCellWithReuseIdentifier: "GraySquareCell")
+        collectionView.register(GraySquareCell.self, forCellWithReuseIdentifier: "GraySquareCell")
+        collectionView.register(SectionHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "sectionHeader") // 여기를 수정했습니다.
+
         collectionView.isScrollEnabled = true
         collectionView.showsVerticalScrollIndicator = false
         return collectionView
@@ -65,23 +83,52 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
     }
 
     // 컴포지셔널 레이아웃을 생성하는 메소드
-    func createCompositionalLayout() -> UICollectionViewLayout {
-        // Main Exhibition Item
-        let mainExhibitionItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                            heightDimension: .absolute(360)) // MainExhibitionCollectionCell의 높이
-        let mainExhibitionItem = NSCollectionLayoutItem(layoutSize: mainExhibitionItemSize)
+    let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
+        if sectionIndex == 0 {
+            // Main Exhibition Item
+            let mainExhibitionItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                heightDimension: .absolute(360)) // MainExhibitionCollectionCell의 높이
+            let mainExhibitionItem = NSCollectionLayoutItem(layoutSize: mainExhibitionItemSize)
 
-        // Main Exhibition Group
-        let mainExhibitionGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                             heightDimension: .absolute(360))
-        let mainExhibitionGroup = NSCollectionLayoutGroup.horizontal(layoutSize: mainExhibitionGroupSize, subitems: [mainExhibitionItem])
+            // Main Exhibition Group
+            let mainExhibitionGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                 heightDimension: .absolute(360))
+            let mainExhibitionGroup = NSCollectionLayoutGroup.horizontal(layoutSize: mainExhibitionGroupSize, subitems: [mainExhibitionItem])
 
-        // Main Exhibition Section
-        let section = NSCollectionLayoutSection(group: mainExhibitionGroup)
-        section.orthogonalScrollingBehavior = .groupPaging // 여기에서 가로 스크롤을 설정합니다.
+            // Main Exhibition Section
+            let section = NSCollectionLayoutSection(group: mainExhibitionGroup)
+            section.orthogonalScrollingBehavior = .groupPaging // 여기에서 가로 스크롤을 설정합니다.
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0) // 첫 번째 섹션의 아래 간격을 10으로 설정
 
-        return UICollectionViewCompositionalLayout(section: section)
+            return section // 이 부분이 누락되어 있었습니다.
+
+        }else {
+            // Second section (Gray Square Cells)
+
+            let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(130),
+                                                  heightDimension: .absolute(155))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0)
+
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(130),
+                                                   heightDimension: .absolute(155))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+            let section = NSCollectionLayoutSection(group: group)
+
+            // 섹션을 생성한 후에 헤더를 설정합니다.
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+            let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+            section.boundarySupplementaryItems = [header]
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0)
+
+            section.interGroupSpacing = 16
+            section.orthogonalScrollingBehavior = .continuous
+
+            return section
+        }
     }
+
 
     private func bindCollectionView() {
         Observable.just(items)
@@ -102,22 +149,59 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
         MainExhibitionCollectionView.snp.makeConstraints { make in
             make.top.equalTo(CategoryCollectionView.snp.bottom).offset(20)
             make.left.right.equalToSuperview()
-            make.height.equalTo(380)
+            make.height.equalTo(1000)
         }
     }
 
     private func bindNewCollectionView() {
         // 예제 데이터
-        let newItems = Array(repeating: "리암 길릭: \n The Alterants", count: 10)
+        let firstSectionItems = Array(repeating: "리암 길릭: \n The Alterants", count: 10)
+         let secondSectionItems = Array(repeating: "GraySquare", count: 10)
+         let thirdSectionItems = Array(repeating: "GraySquare", count: 10) // 세 번째 섹션 데이터
+         let sections = [
+             SectionItem(header: "MainExhibition", items: firstSectionItems),
+             SectionItem(header: "GraySquare", items: secondSectionItems),
+             SectionItem(header: "GraySquare", items: thirdSectionItems) // 세 번째 섹션 추가
+         ]
 
-        Observable.just(newItems)
-            .bind(to: MainExhibitionCollectionView.rx.items(cellIdentifier: "MainExhibitionCollectionCell", cellType: MainExhibitionCollectionCell.self)) { (row, text, cell) in
-                cell.label.text = text
-                cell.contentView.clipsToBounds = true
-                cell.setImage(image: UIImage(named: "Rectangle")) // 각 셀에 이미지 설정
-                self.adjustCellLayoutForEvenItems(cell: cell, indexPath: IndexPath(row: row, section: 0))
-            }.disposed(by: disposeBag)
 
+        // 2. 데이터 소스를 설정합니다.
+        let dataSource = RxCollectionViewSectionedReloadDataSource<SectionItem>(
+            configureCell: { (dataSource, collectionView, indexPath, item) -> UICollectionViewCell in
+                if indexPath.section == 0 {
+                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainExhibitionCollectionCell", for: indexPath) as! MainExhibitionCollectionCell
+                    cell.label.text = item
+                    cell.contentView.clipsToBounds = true
+                    cell.setImage(image: UIImage(named: "Rectangle"))
+                    self.adjustCellLayoutForEvenItems(cell: cell, indexPath: indexPath)
+                    return cell
+                } else {
+                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GraySquareCell", for: indexPath) as! GraySquareCell
+                    // 필요한 설정을 추가할 수 있습니다.
+                    return cell
+                }
+            },
+            configureSupplementaryView: { (dataSource, collectionView, kind, indexPath) -> UICollectionReusableView in
+                if kind == UICollectionView.elementKindSectionHeader {
+                    let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "sectionHeader", for: indexPath) as! SectionHeader
+                    if indexPath.section == 1 {
+                        header.label.text = "용인 근처의 전시"
+                    } else if indexPath.section == 2 {
+                        header.label.text = "도트 님의 취향 저격 콘텐츠"
+                    }
+                    return header
+                }
+                return UICollectionReusableView()
+            
+
+            }
+
+            
+        )
+
+        Observable.just(sections)
+            .bind(to: MainExhibitionCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
 
         MainExhibitionCollectionView.rx.itemSelected.subscribe(onNext: { indexPath in
             print("Selected new item at \(indexPath.row)")
@@ -174,11 +258,50 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
         if collectionView == CategoryCollectionView {
             return CGSize(width: 73, height: 34)
         } else if collectionView == MainExhibitionCollectionView {
-            return CGSize(width: UIScreen.main.bounds.width, height: 360) // 화면의 가로길이를 셀의 가로길이로 설정
+            if indexPath.section == 0 {
+                return CGSize(width: UIScreen.main.bounds.width, height: 360) // 첫 번째 섹션의 셀 크기
+            } else if indexPath.section == 1 {
+                return CGSize(width: 50, height: 100) // 두 번째 섹션의 셀 크기
+            }
         }
         return CGSize.zero
     }
 
+}
 
+class GraySquareCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .gray // 회색 배경 설정
+
+        // 추가적인 UI 구성이나 데이터 바인딩이 필요하다면 여기에 작성하시면 됩니다.
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class SectionHeader: UICollectionReusableView {
+    let label = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(label)
+
+        // 폰트 사이즈와 스타일 설정
+        label.font = UIFont.boldSystemFont(ofSize: 20) // 여기에서 폰트 사이즈와 스타일을 조정합니다.
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.textAlignment = .left
+        label.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true // 왼쪽 정렬
+        label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
