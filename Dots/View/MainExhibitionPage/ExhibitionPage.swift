a78556c9805ab930d699a23f2ea70beb810dd617
cheshire0105 <cheshire0105@gmail.com>
2023-10-27 15:50:56 +0900

    [feat] : 전시 페이지 안에 스크롤 뷰와 그 안에 이미지를 넣었음 상단 이미지만 - 레이블 추가 예정

--- Dots/View/MainExhibitionPage/ExhibitionPage.swift
+++ Dots/View/MainExhibitionPage/ExhibitionPage.swift
@@ -10,6 +10,10 @@ import SnapKit
 
 class ExhibitionPage: UIViewController {
 
+    // 스크롤 뷰와 콘텐츠 뷰를 정의합니다.
+       let scrollView = UIScrollView()
+       let contentView = UIView()
+    let exhibitionImageView = UIImageView() // 이미지 뷰를 정의합니다.
 
 
 
@@ -30,6 +34,45 @@ class ExhibitionPage: UIViewController {
 
         setupBackButton()
         setupRightBarButton()
+
+        setupScrollView()
+        setupExhibitionImage()
+
+    }
+
+    private func setupExhibitionImage() {
+        // 이미지 뷰를 contentView에 추가합니다.
+        contentView.addSubview(exhibitionImageView)
+
+        // 이미지 뷰의 제약 조건을 설정합니다.
+        exhibitionImageView.snp.makeConstraints { make in
+            make.top.equalTo(contentView) // 상단에서 16픽셀 떨어진 위치에 배치
+            make.left.equalTo(contentView) // 왼쪽에서 16픽셀 떨어진 위치에 배치
+            make.right.equalTo(contentView) // 오른쪽에서 16픽셀 떨어진 위치에 배치
+            make.height.equalTo(400)
+        }
+
+        // 이미지 뷰에 이미지를 설정합니다. (원하는 이미지로 변경하실 수 있습니다.)
+        exhibitionImageView.image = UIImage(named: "ExhibitionPageBack")
+        exhibitionImageView.contentMode = .scaleAspectFill // 이미지의 콘텐츠 모드를 설정합니다.
+        exhibitionImageView.clipsToBounds = true
+    }
+
+    // 스크롤 뷰 설정
+    private func setupScrollView() {
+        // 스크롤 뷰를 뷰에 추가
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview() // 스크롤 뷰를 전체 화면으로 설정
+        }
+
+        // 콘텐츠 뷰를 스크롤 뷰에 추가
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints { (make) in
+            make.top.equalTo(view) // 변경된 부분: 콘텐츠 뷰의 상단을 뷰의 상단에 맞춤
+            make.left.right.bottom.equalToSuperview()
+            make.width.equalTo(scrollView) // 콘텐츠 뷰의 너비를 스크롤 뷰의 너비와 같게 설정
+        }
     }
 
 
