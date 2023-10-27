38e7a4fa0700b7d9c41e3e0eef95e4ea4d3fa661
cheshire0105 <cheshire0105@gmail.com>
2023-10-27 22:20:36 +0900

    [feat] : 상세 페이지 전시 제목, 미술관 이름, 날짜, 위치 아이콘과 레이아웃 정의

--- Dots/View/MainExhibitionPage/ExhibitionPage.swift
+++ Dots/View/MainExhibitionPage/ExhibitionPage.swift
@@ -11,11 +11,19 @@ import SnapKit
 class ExhibitionPage: UIViewController {
 
     // 스크롤 뷰와 콘텐츠 뷰를 정의합니다.
-       let scrollView = UIScrollView()
-       let contentView = UIView()
+    let scrollView = UIScrollView()
+    let contentView = UIView()
     let exhibitionImageView = UIImageView() // 이미지 뷰를 정의합니다.
+    let titleLabel = UILabel()
+    let galleryButtonLabel = UILabel()
 
+    let iconImageView = UIImageView()
+    let descriptionLabel = UILabel()
+    let stackView = UIStackView()
 
+    let iconImageViewTwo = UIImageView()
+    let descriptionLabelTwo = UILabel()
+    let stackViewTwo = UIStackView()
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -45,6 +53,17 @@ class ExhibitionPage: UIViewController {
     private func setupExhibitionImage() {
         // 이미지 뷰를 contentView에 추가합니다.
         contentView.addSubview(exhibitionImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(galleryButtonLabel)
+
+        stackView.addArrangedSubview(iconImageView)
+        stackView.addArrangedSubview(descriptionLabel)
+        contentView.addSubview(stackView)
+
+        stackViewTwo.addArrangedSubview(iconImageViewTwo)
+        stackViewTwo.addArrangedSubview(descriptionLabelTwo)
+        contentView.addSubview(stackViewTwo)
+
 
         // 이미지 뷰의 제약 조건을 설정합니다.
         exhibitionImageView.snp.makeConstraints { make in
@@ -54,10 +73,63 @@ class ExhibitionPage: UIViewController {
             make.height.equalTo(400)
         }
 
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView).offset(200) // 이미지 아래에 10픽셀 떨어진 위치에 배치
+            make.left.equalTo(contentView).offset(16) // 왼쪽에서 16픽셀 떨어진 위치에 배치
+            make.right.equalTo(contentView).offset(-16) // 오른쪽에서 16픽셀 떨어진 위치에 배치
+        }
+
+        galleryButtonLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(13) // titleLabel 아래에 20픽셀 떨어진 위치에 배치
+            make.left.equalTo(contentView).offset(16) // 왼쪽에서 16픽셀 떨어진 위치에 배치
+            make.right.equalTo(contentView).offset(-16) // 오른쪽에서 16픽셀 떨어진 위치에 배치
+        }
+
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(galleryButtonLabel.snp.bottom).offset(13)
+            make.left.equalTo(contentView).offset(16)
+            make.right.lessThanOrEqualTo(contentView).offset(-16)
+        }
+
+        stackViewTwo.snp.makeConstraints { make in
+            make.top.equalTo(stackView.snp.bottom).offset(13)
+            make.left.equalTo(contentView).offset(16)
+            make.right.lessThanOrEqualTo(contentView).offset(-16)
+        }
+
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .center
+
+        stackViewTwo.axis = .horizontal
+        stackViewTwo.spacing = 10
+        stackViewTwo.alignment = .center
+
+
         // 이미지 뷰에 이미지를 설정합니다. (원하는 이미지로 변경하실 수 있습니다.)
         exhibitionImageView.image = UIImage(named: "ExhibitionPageBack")
         exhibitionImageView.contentMode = .scaleAspectFill // 이미지의 콘텐츠 모드를 설정합니다.
         exhibitionImageView.clipsToBounds = true
+
+        titleLabel.text = "리암 길릭 : The Alterants"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 24) // 폰트 크기를 24로 설정하고 볼드체로 지정
+        titleLabel.textColor = .white
+
+        galleryButtonLabel.text = "갤러리바튼"
+        galleryButtonLabel.font = UIFont.systemFont(ofSize: 16) // 폰트 크기를 20로 설정하고 일반 체로 지정
+        galleryButtonLabel.textColor = .white
+        
+        iconImageView.image = UIImage(named: "exhibitionGeoIcon")
+        iconImageView.contentMode = .scaleAspectFit
+        descriptionLabel.text = "서울 종로구 삼청로 30"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 12)
+        descriptionLabel.textColor = .white
+
+        iconImageViewTwo.image = UIImage(named: "exhibitionCalendarIcon")
+        iconImageViewTwo.contentMode = .scaleAspectFit
+        descriptionLabelTwo.text = "~ 2024.02.12"
+        descriptionLabelTwo.font = UIFont.systemFont(ofSize: 12)
+        descriptionLabelTwo.textColor = .white
     }
 
     // 스크롤 뷰 설정
@@ -99,6 +171,5 @@ class ExhibitionPage: UIViewController {
     }
 
     @objc func rightButtonTapped() {
-        // 여기에 원하는 액션을 추가합니다.
     }
 }
