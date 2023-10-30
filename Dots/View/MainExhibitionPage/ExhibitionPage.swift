35ec9210b68b94c8d3fa3600482ab3abd23bcf74
cheshire0105 <cheshire0105@gmail.com>
2023-10-30 23:02:23 +0900

    [fix] : 메인 전시 페이지 코드 정리

--- Dots/View/MainExhibitionPage/ExhibitionPage.swift
+++ Dots/View/MainExhibitionPage/ExhibitionPage.swift
@@ -10,10 +10,12 @@ import SnapKit
 
 class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, UIScrollViewDelegate {
 
-    // 스크롤 뷰와 콘텐츠 뷰를 정의합니다.
+    // MARK: - 변수들
+
     let scrollView = UIScrollView()
     let contentView = UIView()
-    let exhibitionImageView = UIImageView() // 이미지 뷰를 정의합니다.
+
+    let exhibitionImageView = UIImageView()
     let titleLabel = UILabel()
     let galleryButtonLabel = UILabel()
 
@@ -26,23 +28,22 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
     let stackViewTwo = UIStackView()
 
     private lazy var firstPageViewController: FirstPageViewController = {
-           let viewController = FirstPageViewController()
-           return viewController
-       }()
+        let viewController = FirstPageViewController()
+        return viewController
+    }()
 
-    // 세그먼트 컨트롤을 생성합니다.
     private let segmentedControl: MyPageSegmentedControl = {
-        let items = ["후기", "전시 정보"] // 세그먼트의 아이템들을 설정합니다.
-        let segmentedControl = MyPageSegmentedControl(items: items) // 세그먼트 컨트롤을 초기화합니다.
-        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .normal) // 일반 상태의 타이틀 색상을 설정합니다.
+        let items = ["후기", "전시 정보"]
+        let segmentedControl = MyPageSegmentedControl(items: items)
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .normal)
         segmentedControl.setTitleTextAttributes(
             [
                 NSAttributedString.Key.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
-                .font: UIFont.systemFont(ofSize: 13, weight: .semibold) // 선택된 상태의 타이틀 색상과 폰트를 설정합니다.
+                .font: UIFont.systemFont(ofSize: 13, weight: .semibold)
             ],
             for: .selected
         )
-        segmentedControl.selectedSegmentIndex = 0 // 첫 번째 세그먼트를 기본으로 선택합니다.
+        segmentedControl.selectedSegmentIndex = 0
         return segmentedControl
     }()
 
@@ -50,7 +51,7 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
         let pageVC = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         pageVC.dataSource = self
         pageVC.delegate = self
-        pageVC.view.backgroundColor = .yellow // 배경색을 설정해 줍니다.
+        pageVC.view.backgroundColor = .yellow
 
         return pageVC
     }()
@@ -61,78 +62,59 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
         return [firstVC, secondVC]
     }()
 
-    @objc private func didChangeSegmentControl(_ sender: UISegmentedControl) {
-        let direction: UIPageViewController.NavigationDirection = (pageViewController.viewControllers?.first == viewControllers[sender.selectedSegmentIndex]) ? .reverse : .forward
-        pageViewController.setViewControllers([viewControllers[sender.selectedSegmentIndex]], direction: direction, animated: true, completion: nil)
-    }
 
-    //    let mySegmentedControl = MyPageSegmentedControl(items: ["페이지 1", "페이지 2"])
-
+    // MARK: - 뷰의 생명주기
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.contentInsetAdjustmentBehavior = .never
-
         scrollView.delegate = self
 
-
-        // 1. 기본 뷰 설정
-        // 네비게이션 바의 배경을 투명하게 설정
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.isTranslucent = true
         self.view.backgroundColor = .black
+
         setupBackButton()
         setupRightBarButton()
-
-        // 2. UI 구성요소 설정
-        // 스크롤 뷰와 관련된 설정
-        //        setupScrollView()
-        // 전시 이미지와 관련된 뷰 설정
         setupExhibitionImage()
-        // 세그먼트 컨트롤의 값이 변경되었을 때 didChangeSegmentControl 메서드를 호출하도록 설정
         segmentedControl.addTarget(self, action: #selector(didChangeSegmentControl), for: .valueChanged)
-
         setupScrollView()
 
-        // 3. UIPageViewController 설정
         addChild(pageViewController)
-        contentView.addSubview(pageViewController.view)  // pageViewController의 뷰를 contentView에 추가합니다.
+        contentView.addSubview(pageViewController.view)
         pageViewController.didMove(toParent: self)
         pageViewController.setViewControllers([viewControllers[0]], direction: .forward, animated: true, completion: nil)
-        setupPageViewControllerConstraints() // 제약 조건 설정
+        setupPageViewControllerConstraints()
 
-        // FirstPageViewController를 자식 뷰 컨트롤러로 추가
-              addChild(firstPageViewController)
-              scrollView.addSubview(firstPageViewController.view)
-              firstPageViewController.didMove(toParent: self)
+        addChild(firstPageViewController)
+        scrollView.addSubview(firstPageViewController.view)
+        firstPageViewController.didMove(toParent: self)
 
-              // FirstPageViewController의 뷰에 대한 레이아웃 설정
-              firstPageViewController.view.snp.makeConstraints { make in
-                  make.top.equalTo(segmentedControl.snp.bottom).offset(10)
-                  make.left.right.equalTo(scrollView)
-                  make.width.equalTo(scrollView)  // 스크롤 뷰의 너비와 동일하게 설정
-                  make.height.equalTo(1000)  // 원하는 높이 값 설정
-                  make.bottom.equalTo(scrollView)  // 이 줄을 추가하여 스크롤 뷰의 bottom과 연결
-              }
+        firstPageViewController.view.snp.makeConstraints { make in
+            make.top.equalTo(segmentedControl.snp.bottom).offset(10)
+            make.left.right.equalTo(scrollView)
+            make.width.equalTo(scrollView)
+            make.height.equalTo(1000)
+            make.bottom.equalTo(scrollView)
+        }
     }
 
+    // MARK: - 함수들
 
-
-
-
-
-
-
-
+    // 세그먼트의 값이 변경 되었을 때
+    @objc private func didChangeSegmentControl(_ sender: UISegmentedControl) {
+        let direction: UIPageViewController.NavigationDirection = (pageViewController.viewControllers?.first == viewControllers[sender.selectedSegmentIndex]) ? .reverse : .forward
+        pageViewController.setViewControllers([viewControllers[sender.selectedSegmentIndex]], direction: direction, animated: true, completion: nil)
+    }
+    
+    // 세그먼트 컨트롤에서 보여지는 PageView의 레이아웃
     private func setupPageViewControllerConstraints() {
         pageViewController.view.snp.makeConstraints { make in
             make.top.equalTo(segmentedControl.snp.bottom).offset(10)
@@ -140,18 +122,16 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
             make.height.equalTo(view.snp.height)
         }
     }
-
-
-
-
-
+    
+    // 페이지 컨트롤러의 첫번째 인덱스
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         if let index = viewControllers.firstIndex(of: viewController), index > 0 {
             return viewControllers[index - 1]
         }
         return nil
     }
-
+    
+    // 페이지 컨트롤러의 두번째 인덱스
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         if let index = viewControllers.firstIndex(of: viewController), index < viewControllers.count - 1 {
             return viewControllers[index + 1]
@@ -164,52 +144,87 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
             segmentedControl.selectedSegmentIndex = index
         }
     }
+    
+    // 스크롤 뷰의 레이아웃을 정하는 함수 - 스크롤 뷰 안에 콘텐츠 뷰를 동일하게 추가
+    private func setupScrollView() {
 
+        view.addSubview(scrollView)
 
+        scrollView.snp.makeConstraints { (make) in
+            make.top.right.left.bottom.equalTo(view)
+        }
 
+        scrollView.addSubview(contentView)
 
+        contentView.snp.remakeConstraints { (make) in
+            make.top.equalTo(scrollView)
+            make.left.right.equalTo(scrollView)
+            make.bottom.equalTo(pageViewController.view.snp.bottom)
+        }
+    }
 
-
-
+    // 상단의 전시 이미지를 설정 하는 함수
     private func setupExhibitionImage() {
         // 이미지 뷰를 contentView에 추가합니다.
+
+        // 전시 이미지와 레이블을 추가
         contentView.addSubview(exhibitionImageView)
         contentView.addSubview(titleLabel)
         contentView.addSubview(galleryButtonLabel)
-
+        
+        // 그 밑에 위치와 날짜 레이블의 스택뷰를 추가
         stackView.addArrangedSubview(iconImageView)
         stackView.addArrangedSubview(descriptionLabel)
         contentView.addSubview(stackView)
+
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.alignment = .center
 
         stackViewTwo.addArrangedSubview(iconImageViewTwo)
         stackViewTwo.addArrangedSubview(descriptionLabelTwo)
         contentView.addSubview(stackViewTwo)
 
+        stackViewTwo.axis = .horizontal
+        stackViewTwo.spacing = 10
+        stackViewTwo.alignment = .center
+
+        // 세그먼트 컨트롤을 추가
         contentView.addSubview(segmentedControl)
 
+        // 세그먼트 컨트롤로 연결 되는 페이지 뷰를 추가
         contentView.addSubview(pageViewController.view)
 
-
-        // 이미지 뷰의 제약 조건을 설정합니다.
         exhibitionImageView.snp.updateConstraints { make in
-            make.top.equalTo(contentView) // 상단에서 16픽셀 떨어진 위치에 배치
-            make.left.equalTo(contentView) // 왼쪽에서 16픽셀 떨어진 위치에 배치
-            make.right.equalTo(contentView) // 오른쪽에서 16픽셀 떨어진 위치에 배치
+            make.top.equalTo(contentView)
+            make.left.equalTo(contentView)
+            make.right.equalTo(contentView)
             make.height.equalTo(350)
         }
 
+        exhibitionImageView.image = UIImage(named: "morningStar")
+        exhibitionImageView.contentMode = .scaleAspectFill
+        exhibitionImageView.clipsToBounds = true
 
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(contentView).offset(200) // 이미지 아래에 10픽셀 떨어진 위치에 배치
-            make.left.equalTo(contentView).offset(16) // 왼쪽에서 16픽셀 떨어진 위치에 배치
-            make.right.equalTo(contentView).offset(-16) // 오른쪽에서 16픽셀 떨어진 위치에 배치
+            make.top.equalTo(contentView).offset(200)
+            make.left.equalTo(contentView).offset(16)
+            make.right.equalTo(contentView).offset(-16)
         }
 
+        titleLabel.text = "리암 길릭 : The Alterants"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        titleLabel.textColor = .white
+
         galleryButtonLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(13) // titleLabel 아래에 20픽셀 떨어진 위치에 배치
-            make.left.equalTo(contentView).offset(16) // 왼쪽에서 16픽셀 떨어진 위치에 배치
-            make.right.equalTo(contentView).offset(-16) // 오른쪽에서 16픽셀 떨어진 위치에 배치
+            make.top.equalTo(titleLabel.snp.bottom).offset(13)
+            make.left.equalTo(contentView).offset(16)
+            make.right.equalTo(contentView).offset(-16)
         }
+
+        galleryButtonLabel.text = "갤러리바튼"
+        galleryButtonLabel.font = UIFont.systemFont(ofSize: 16)
+        galleryButtonLabel.textColor = .white
 
         stackView.snp.makeConstraints { make in
             make.top.equalTo(galleryButtonLabel.snp.bottom).offset(13)
@@ -217,49 +232,17 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
             make.right.lessThanOrEqualTo(contentView).offset(-16)
         }
 
-        stackViewTwo.snp.makeConstraints { make in
-            make.top.equalTo(stackView.snp.bottom).offset(13)
-            make.left.equalTo(contentView).offset(16)
-            make.right.lessThanOrEqualTo(contentView).offset(-16)
-        }
-
-        // MyPageSegmentedControl 설정
-        segmentedControl.snp.makeConstraints { make in
-            make.top.equalTo(exhibitionImageView.snp.bottom).offset(16)
-            make.left.equalTo(contentView).offset(16)
-            make.height.equalTo(40)  // 높이 지정
-        }
-
-
-
-
-        stackView.axis = .horizontal
-        stackView.spacing = 10
-        stackView.alignment = .center
-
-        stackViewTwo.axis = .horizontal
-        stackViewTwo.spacing = 10
-        stackViewTwo.alignment = .center
-
-
-        // 이미지 뷰에 이미지를 설정합니다. (원하는 이미지로 변경하실 수 있습니다.)
-        exhibitionImageView.image = UIImage(named: "morningStar")
-        exhibitionImageView.contentMode = .scaleAspectFill // 이미지의 콘텐츠 모드를 설정합니다.
-        exhibitionImageView.clipsToBounds = true
-
-        titleLabel.text = "리암 길릭 : The Alterants"
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 24) // 폰트 크기를 24로 설정하고 볼드체로 지정
-        titleLabel.textColor = .white
-
-        galleryButtonLabel.text = "갤러리바튼"
-        galleryButtonLabel.font = UIFont.systemFont(ofSize: 16) // 폰트 크기를 20로 설정하고 일반 체로 지정
-        galleryButtonLabel.textColor = .white
-
         iconImageView.image = UIImage(named: "exhibitionGeoIcon")
         iconImageView.contentMode = .scaleAspectFit
         descriptionLabel.text = "서울 종로구 삼청로 30"
         descriptionLabel.font = UIFont.systemFont(ofSize: 12)
         descriptionLabel.textColor = .white
+
+        stackViewTwo.snp.makeConstraints { make in
+            make.top.equalTo(stackView.snp.bottom).offset(13)
+            make.left.equalTo(contentView).offset(16)
+            make.right.lessThanOrEqualTo(contentView).offset(-16)
+        }
 
         iconImageViewTwo.image = UIImage(named: "exhibitionCalendarIcon")
         iconImageViewTwo.contentMode = .scaleAspectFit
@@ -267,37 +250,13 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
         descriptionLabelTwo.font = UIFont.systemFont(ofSize: 12)
         descriptionLabelTwo.textColor = .white
 
-
-
-    }
-
-
-
-
-    private func setupScrollView() {
-        view.addSubview(scrollView)
-        scrollView.snp.makeConstraints { (make) in
-            make.top.equalTo(view.snp.top) // 화면 최상단에 맞춤
-            make.left.right.bottom.equalTo(view)
+        segmentedControl.snp.makeConstraints { make in
+            make.top.equalTo(exhibitionImageView.snp.bottom).offset(16)
+            make.left.equalTo(contentView).offset(16)
+            make.height.equalTo(40)  // 높이 지정
         }
 
-        scrollView.addSubview(contentView)
-
-        contentView.snp.remakeConstraints { (make) in
-            make.top.equalTo(scrollView)
-            make.left.right.equalTo(view)
-            make.width.equalTo(scrollView)  // contentView의 너비를 scrollView와 동일하게 설정
-            make.bottom.equalTo(pageViewController.view.snp.bottom)  // 중요: contentView의 하단을 pageViewController의 뷰의 하단에 맞춤
-        }
     }
-
-
-
-
-
-
-
-
 
     private func setupBackButton() {
         let backButtonImage = UIImage(named: "backButton")
@@ -318,6 +277,7 @@ class ExhibitionPage: UIViewController, UIPageViewControllerDataSource, UIPageVi
     }
 
     @objc func rightButtonTapped() {
+
     }
 }
 
