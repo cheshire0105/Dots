d5e42ae115f5bb7ac1b438510ea8363c3f17996b
cheshire0105 <cheshire0105@gmail.com>
2023-11-02 22:10:46 +0900

    [feat] : 서치바 밑에 카테고리 버튼 추가

--- Dots/View/SearchPage/SearchPage.swift
+++ Dots/View/SearchPage/SearchPage.swift
@@ -8,57 +8,134 @@
 import UIKit
 import SnapKit
 
-class SearchPage: UIViewController, UITextFieldDelegate {
+class SearchPage: UIViewController, UISearchBarDelegate {
 
-    let searchTextField = UITextField()
+    let searchBar = UISearchBar()
+    let popularButton = UIButton()
+    let exhibitionButton = UIButton()
+    let artistButton = UIButton()
+
+    let separatorLine = UIView()
+
+    let highlightView = UIView()
+    var selectedButton: UIButton?
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupSearchTextField()
+        setupSearchBar()
+        setupButtons()
+        setupSeparatorLine()
+        setupHighlightView()
+        selectButton(popularButton)  // 초기 선택
+
+
     }
 
-    func setupSearchTextField() {
-        searchTextField.delegate = self
-        let placeholderAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 14),
-            .foregroundColor: UIColor.gray
-        ]
-        searchTextField.attributedPlaceholder = NSAttributedString(string: "전시, 작가를 검색하세요.", attributes: placeholderAttributes)
-        searchTextField.borderStyle = .roundedRect
-        searchTextField.returnKeyType = .search
-        searchTextField.backgroundColor = UIColor.darkGray
-        view.addSubview(searchTextField)
+    func setupHighlightView() {
+        highlightView.backgroundColor = .white
+        view.addSubview(highlightView)
 
-        let iconImageView = UIImageView(image: UIImage(named: "Search Glyph"))
-        iconImageView.contentMode = .scaleAspectFit
-        let iconContainerView = UIView()
-        iconContainerView.addSubview(iconImageView)
-        iconImageView.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(10)  // 왼쪽 패딩 추가
-            make.centerY.equalToSuperview().offset(16)
-            make.width.equalTo(25)
-            make.height.equalTo(15)
+        highlightView.snp.makeConstraints { make in
+            make.top.equalTo(separatorLine.snp.bottom)
+            make.height.equalTo(2)
+            make.width.equalTo(popularButton)
+            make.centerX.equalTo(popularButton)
         }
-        searchTextField.leftView = iconContainerView
-        searchTextField.leftViewMode = .always
-        iconContainerView.snp.makeConstraints { make in
-            make.width.equalTo(35)  // 컨테이너 뷰의 너비 조절
+    }
+
+    func selectButton(_ button: UIButton) {
+        selectedButton?.setTitleColor(.lightGray, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        selectedButton = button
+
+        highlightView.snp.remakeConstraints { make in
+            make.top.equalTo(separatorLine.snp.bottom)
+            make.height.equalTo(2)
+            make.width.equalTo(button)
+            make.centerX.equalTo(button)
         }
 
-        searchTextField.snp.makeConstraints { make in
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func buttonClicked(_ sender: UIButton) {
+        selectButton(sender)
+    }
+
+
+    func setupSearchBar() {
+        searchBar.delegate = self
+        searchBar.placeholder = "전시, 작가를 검색하세요."
+        searchBar.searchBarStyle = .minimal
+        searchBar.backgroundColor = UIColor.black
+        view.addSubview(searchBar)
+
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+            textField.font = UIFont.systemFont(ofSize: 15)  // 폰트 크기 설정
+            textField.textColor = .white
+            textField.backgroundColor = .black
+            textField.attributedPlaceholder = NSAttributedString(string: "전시, 작가를 검색하세요.", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+        }
+
+        searchBar.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
-            make.leading.equalTo(view).offset(10)
-            make.trailing.equalTo(view).offset(-10)
+            make.leading.equalTo(view).offset(5)
+            make.trailing.equalTo(view).offset(-5)
+        }
+    }
+
+    func setupButtons() {
+        let buttons = [popularButton, exhibitionButton, artistButton]
+        let titles = ["인기", "전시", "작가"]
+
+        for (button, title) in zip(buttons, titles) {
+            button.setTitle(title, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 18)  // 폰트 크기 설정
+            button.backgroundColor = .black
+            button.setTitleColor(.lightGray, for: .normal)
+            button.addTarget(self, action: #selector(buttonClicked(_:)), for: .touchUpInside)
+            view.addSubview(button)
+        }
+
+        popularButton.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(10)
+            make.leading.equalTo(view).offset(20)
+        }
+
+        exhibitionButton.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(10)
+            make.leading.equalTo(popularButton.snp.trailing).offset(20)
+        }
+
+        artistButton.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(10)
+            make.leading.equalTo(exhibitionButton.snp.trailing).offset(20)
         }
     }
 
 
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()  // 키보드 숨기기
-        print("Search: \(textField.text ?? "")")
+    func setupSeparatorLine() {
+        separatorLine.backgroundColor = .lightGray
+        view.addSubview(separatorLine)
+
+        separatorLine.snp.makeConstraints { make in
+            make.top.equalTo(popularButton.snp.bottom).offset(10)  // 버튼들 아래에 위치
+            make.leading.trailing.equalTo(view)  // 뷰의 양쪽 가장자리에 맞춤
+            make.height.equalTo(1)  // 선의 높이를 1로 설정하여 얇은 선이 되도록 함
+        }
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()  // 키보드 숨기기
+        print("Search: \(searchBar.text ?? "")")
         // 검색 로직을 여기에 추가하세요.
-        return true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()  // 키보드 숨기기
     }
 }
-
-
