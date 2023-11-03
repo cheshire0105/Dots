c4f6cf9f38f19213ee0e6fbfc51a1b76aa352394
judas <churoo0418@gmail.com>
2023-11-03 20:22:30 +0900

    [판모달_UI업데이트] 판모달 ui 수정 + 신규 ui 추가 댓글관련 UI

--- Dots/View/PopularReviewsPage/PopularReviewDetailPanModal.swift
+++ Dots/View/PopularReviewsPage/PopularReviewDetailPanModal.swift
@@ -5,16 +5,12 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-class PopularReviewDetailPanModal : UIViewController {
+class PopularReviewDetailPanModal : UIViewController, UITableViewDelegate {
     
-    private let 상단_블록 = {
-        let uiView = UIView()
-        uiView.backgroundColor = UIColor.darkGray.withAlphaComponent(0.8)
-        return uiView
-    } ()
     private let 판모달_댓글_테이블뷰 = {
         let tableView = UITableView()
-        tableView.backgroundColor = UIColor.darkGray.withAlphaComponent(0.8)
+//      tableView.backgroundColor = UIColor.darkGray.withAlphaComponent(0.6)
+        tableView.backgroundColor = UIColor.clear
         tableView.isUserInteractionEnabled = true
         tableView.showsVerticalScrollIndicator = false
         tableView.separatorStyle = .none
@@ -22,20 +18,16 @@ class PopularReviewDetailPanModal : UIViewController {
         return tableView
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        Ui레이아웃()
+        view.backgroundColor = UIColor.darkGray.withAlphaComponent(0.6)
+
         테이블뷰_레이아웃()
+        판모달_댓글_테이블뷰.delegate = self
+        판모달_댓글_테이블뷰.dataSource = self
+        판모달_댓글_테이블뷰.register(PopularReviewDetailPanModalCell.self, forCellReuseIdentifier: "PopularReviewDetailPanModalCell")
     }
     
-    func Ui레이아웃 () {
-        view.addSubview(상단_블록)
-        상단_블록.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.trailing.equalToSuperview()
-        }
-    }
     func 테이블뷰_레이아웃 () {
         view.addSubview(판모달_댓글_테이블뷰)
         판모달_댓글_테이블뷰.snp.makeConstraints { make in
@@ -45,7 +37,6 @@ class PopularReviewDetailPanModal : UIViewController {
             make.trailing.equalToSuperview().offset(-12)
         }
     }
-    
 }
 
 
@@ -55,40 +46,38 @@ class PopularReviewDetailPanModal : UIViewController {
 extension PopularReviewDetailPanModal : UITabBarDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        15
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PopularReviewDetailPanModalCell", for: indexPath) as! PopularReviewDetailPanModalCell
         return cell
     }
-    
 }
-
-
-
 
 extension PopularReviewDetailPanModal : PanModalPresentable {
     
     var panScrollable: UIScrollView? {
         return nil
     }
-    
+    var topOffset: CGFloat {
+        return 100
+    }
     var longFormHeight: PanModalHeight {
         return .contentHeight(440)
-        
     }
     var panModalCornerRadius: CGFloat {
         return 20
     }
-    
     var panModalBackgroundColor: UIColor {
         return UIColor.clear
     }
     var allowsTapToDismiss: Bool {
         return true
     }
-    
     var allowsDragToDismiss: Bool {
         return true
     }
--- Dots/View/PopularReviewsPage/PopularReviewDetailPanModalCell.swift
+++ Dots/View/PopularReviewsPage/PopularReviewDetailPanModalCell.swift
@@ -2,21 +2,93 @@ import SnapKit
 import UIKit
 class PopularReviewDetailPanModalCell: UITableViewCell {
     
+    var 댓글_작성자_이미지 = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "cabanel")
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    var 댓글_작성자_이름: UILabel = {
+        let label = UILabel()
+        label.text = "박철우"
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "HelveticaNeue", size: 14)
+        label.textAlignment = .left
+        return label
+    }()
     
+    let 댓글_내용 = {
+        let label = UILabel()
+        label.text = """
+oiv njqweojr njkxz sjl wke njkfsdkrjbkjsn kllfj
+"""
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "HelveticaNeue", size: 16)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .left
+        return label
+    } ()
+    let 댓글_좋아요_버튼 = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "댓글하트on"), for: .normal)
+        button.setImage(UIImage(named: "댓글하트off"), for: .selected)
+        button.isSelected = true
+        
+        return button
+    } ()
     
-    
+    let 댓글_좋아요_카운트 = {
+        let label = UILabel()
+        let 카운팅 : Int = 0
+        label.text = "\(카운팅)"
+        label.font = UIFont(name: "HelveticaNeue", size: 10)
+        label.textColor = UIColor.white
+        return label
+    } ()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.backgroundColor = UIColor.blue
+        self.backgroundColor = UIColor.clear
         
+            contentView.addSubview(댓글_작성자_이미지)
+        contentView.addSubview(댓글_작성자_이름)
+        contentView.addSubview(댓글_내용)
+        addSubview(댓글_좋아요_버튼)
+        contentView.addSubview(댓글_좋아요_카운트)
         
-        
-        
+        댓글_작성자_이미지.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(10)
+            make.leading.equalToSuperview().offset(5)
+            make.bottom.equalToSuperview().offset(-10)
+            make.trailing.equalToSuperview().offset(-360)
+        }
+        댓글_작성자_이름.snp.makeConstraints { make in
+            make.top.equalTo(댓글_작성자_이미지)
+            make.leading.equalTo(댓글_작성자_이미지.snp.trailing).offset(10)
+            make.trailing.equalToSuperview().offset(-35)
+        }
+        댓글_내용.snp.makeConstraints { make in
+            make.top.equalTo(댓글_작성자_이름.snp.bottom).offset(3)
+            make.leading.equalTo(댓글_작성자_이미지.snp.trailing).offset(10)
+            make.trailing.equalToSuperview().offset(-35)
+            make.bottom.equalTo(댓글_작성자_이미지.snp.bottom)
+        }
+        댓글_좋아요_버튼.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(5)
+            make.leading.equalTo(댓글_내용.snp.trailing).offset(5)
+            make.trailing.equalToSuperview().offset(-5)
+            make.bottom.equalToSuperview().offset(-5)
+        }
+        댓글_좋아요_카운트.snp.makeConstraints { make in
+            
+        }
     }
     
     convenience init() {
-        self.init(style: .default, reuseIdentifier: "youtubeCell")
+        self.init(style: .default, reuseIdentifier: "PopularReviewDetailPanModalCell")
     }
     
     @available(*, unavailable)
