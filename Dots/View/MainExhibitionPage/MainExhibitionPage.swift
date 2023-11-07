d1802ef671f07b60d42008670ad5299b447ea8ef
cheshire0105 <cheshire0105@gmail.com>
2023-11-08 00:14:47 +0900

    [기능 추가] : 데이터 임시로 불러오는 로직 추가. 첫번째 메인 섹션에 - 재사용 생각 더 해야 함.

--- Dots/View/MainExhibitionPage/MainExhibitionPage.swift
+++ Dots/View/MainExhibitionPage/MainExhibitionPage.swift
@@ -31,7 +31,9 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
     let disposeBag = DisposeBag()
     var collectionViewTopConstraint: Constraint?
     let items = ["전시회", "미술관", "갤러리", "박물관", "비엔날레"]
-    
+    var exhibitions = [ExhibitionModel]()
+
+
     lazy var CategoryCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.scrollDirection = .horizontal
@@ -93,23 +95,19 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
         
         self.view.backgroundColor = .black
 
-        // ExhibitionModel 배열을 가져오는 함수를 구독하고 결과를 출력한다.
+        // `fetchExhibitions()` 함수를 구독하고 결과를 처리합니다.
         fetchExhibitions()
-            .subscribe(
-                onNext: { exhibitions in
-                    // 데이터 배열을 받으면 출력한다.
-                    print("Received exhibition data: \(exhibitions)")
-                },
-                onError: { error in
-                    // 에러가 발생하면 에러 메시지를 출력한다.
-                    print("An error occurred: \(error)")
-                },
-                onCompleted: {
-                    // 데이터 가져오기 작업이 완료되면 완료 메시지를 출력한다.
-                    print("Fetch completed.")
-                }
-            )
-            .disposed(by: disposeBag) // 메모리 누수를 방지하기 위해 disposeBag에 추가한다.
+            .subscribe(onNext: { [weak self] exhibitions in
+                self?.exhibitions = exhibitions // 데이터를 클래스 프로퍼티에 저장합니다.
+                print(exhibitions)
+                self?.MainExhibitionCollectionView.reloadData() // 컬렉션뷰를 리로드합니다.
+            }, onError: { error in
+                print("An error occurred: \(error)")
+            }, onCompleted: {
+                print("Fetch completed.")
+            })
+            .disposed(by: disposeBag)
+
 
 
     }
@@ -147,12 +145,12 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
             // Second section (Gray Square Cells)
             
             let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(130),
-                                                  heightDimension: .absolute(200))
+                                                  heightDimension: .absolute(250))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
             item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0)
             
             let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(130),
-                                                   heightDimension: .absolute(230))
+                                                   heightDimension: .absolute(200))
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
             
             let section = NSCollectionLayoutSection(group: group)
@@ -211,14 +209,25 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
         
         // 2. 데이터 소스를 설정합니다.
         let dataSource = RxCollectionViewSectionedReloadDataSource<SectionItem>(
-            configureCell: { (dataSource, collectionView, indexPath, item) -> UICollectionViewCell in
+            configureCell: { [weak self] (dataSource, collectionView, indexPath, item) -> UICollectionViewCell in
                 if indexPath.section == 0 {
                     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainExhibitionCollectionCell", for: indexPath) as! MainExhibitionFirstSectionCollectionCell
                     cell.label.text = item
                     cell.contentView.clipsToBounds = true
-                    cell.setImage(image: UIImage(named: "Rectangle"))
-                    self.adjustCellLayoutForEvenItems(cell: cell, indexPath: indexPath)
-                    return cell
+
+                    // 셀에 대한 전시회 데이터를 가져옵니다
+                    // 셀에 대한 전시회 데이터를 가져옵니다.
+                    if let exhibition = self?.exhibitionData(forIndexPath: indexPath) {
+                        // exhibition.poster가 파일명이라고 가정합니다.
+                        let imagePath = "images/서울_전시_1.png" // 예: "images/my_image.jpg"
+                        self?.downloadImage(withPath: imagePath) { image in
+                            DispatchQueue.main.async { // UI 업데이트는 메인 스레드에서 실행합니다.
+                                cell.setImage(image: image) // 셀에 이미지를 설정하는 메소드
+                            }
+                        }
+                    }
+
+    return cell
                 } else {
                     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GraySquareCell", for: indexPath) as! GraySquareCell
                     // 필요한 설정을 추가할 수 있습니다.
@@ -313,7 +322,39 @@ class MainExhibitionPage: UIViewController, UICollectionViewDelegateFlowLayout {
         }
         return CGSize.zero
     }
-    
+
+    func downloadImage(withPath imagePath: String, completion: @escaping (UIImage?) -> Void) {
+        // Firebase Storage의 참조를 얻습니다.
+        let storageRef = Storage.storage().reference(withPath: imagePath)
+
+        // 이미지를 메모리로 직접 다운로드합니다.
+        storageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            if let error = error {
+                print("Error downloading image: \(error)")
+                completion(nil)
+            } else if let data = data, let image = UIImage(data: data) {
+                completion(image)
+            } else {
+                completion(nil)
+            }
+        }
+    }
+
+    func exhibitionData(forIndexPath indexPath: IndexPath) -> ExhibitionModel? {
+        // 첫 번째 섹션에만 전시회 데이터가 표시되므로, 첫 번째 섹션의 인덱스 경로만 처리합니다.
+        if indexPath.section == 0 {
+            // indexPath.item은 배열의 인덱스로 사용됩니다.
+            // exhibitions 배열의 크기를 넘어서는 인덱스에 접근하는 것을 방지합니다.
+            if indexPath.item < exhibitions.count {
+                return exhibitions[indexPath.item]
+            }
+        }
+        // 다른 섹션 또는 잘못된 인덱스에 대해서는 nil을 반환합니다.
+        return nil
+    }
+
+
+
 }
 
 extension MainExhibitionPage: UIScrollViewDelegate {
@@ -333,43 +374,54 @@ extension MainExhibitionPage: UIScrollViewDelegate {
 
 
 class GraySquareCell: UICollectionViewCell {
-    
+
+    var imageView: UIImageView!
     var titleLabel: UILabel!
     var dateLabel: UILabel!
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        self.backgroundColor = .gray // 회색 배경 설정
-        
-        // Title Label 설정
+
+        imageView = UIImageView()
         titleLabel = UILabel()
-        titleLabel.text = "올해의 작가전" // 예제 텍스트
-        titleLabel.textColor = .white
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(titleLabel)
-        
-        // Date Label 설정
         dateLabel = UILabel()
-        dateLabel.text = "~2023.12.34" // 예제 텍스트
+
+        // SnapKit을 사용해 이미지 뷰를 추가합니다.
+        imageView.backgroundColor = .gray
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(imageView.snp.width) // 가로 세로 비율 1:1로 설정
+        }
+
+        // 타이틀 레이블 설정
+        titleLabel.font = .boldSystemFont(ofSize: 16)
+        titleLabel.text = "올해의 전시"
+        titleLabel.textColor = .white
+        contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(10)
+        }
+
+        // 날짜 레이블 설정
+        dateLabel.font = .systemFont(ofSize: 14)
+        dateLabel.text = "2023. 12. 34 ~ 2024. 12. 34"
         dateLabel.textColor = .lightGray
-        dateLabel.font = UIFont.systemFont(ofSize: 14)
-        dateLabel.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(dateLabel)
-        
-        // 제약 조건 설정
-        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: self.bottomAnchor, constant: 10).isActive = true
-        
-        dateLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
-        dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        contentView.addSubview(dateLabel)
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.bottom.lessThanOrEqualToSuperview().offset(-10)
+        }
     }
-    
-    required init?(coder: NSCoder) {
+
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+
 
 
 
@@ -436,11 +488,13 @@ struct ExhibitionModel {
 
 import FirebaseFirestore
 import RxSwift
+import FirebaseStorage
 
 // Firestore에서 전시회 데이터를 가져와서 ExhibitionModel 배열로 변환하는 함수
 func fetchExhibitions() -> Observable<[ExhibitionModel]> {
     // RxSwift의 Observable을 생성하여 비동기 작업을 관리한다.
     return Observable.create { observer in
+        
         // Firestore의 'exhibitions' 컬렉션에 대한 참조를 생성한다.
         let collectionRef = Firestore.firestore().collection("전시")
 
