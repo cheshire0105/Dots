08e510c89454a32b96a2b31cf330dbf728f08d63
judas <churoo0418@gmail.com>
2023-11-14 21:47:05 +0900

    [마이페이지_브랜치]  뷰컨트롤러간 상호 화면전환 가능하게 기초 작업 업데이트 + 각 뷰컨트롤러 페이지 디테일 작업중

--- Dots/View/MyPage/MypageSaved.swift
+++ Dots/View/MyPage/MypageSaved.swift
@@ -1,7 +1,51 @@
 import UIKit
 
 class 마이페이지_보관함 : UIViewController {
-    override func viewDidLoad() {
-        view.backgroundColor = .orange
+    let 뒤로가기_버튼 = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "loginBack"), for: .selected)
+        button.setImage(UIImage(named: ""), for: .normal)
+        button.isSelected = !button.isSelected
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 20
+        return button
+    } ()
+    let 페이지_제목 = {
+        let label = UILabel()
+        label.text = "보관함"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        return label
+    }()
+
+override func viewDidLoad() {
+    view.backgroundColor = .black
+    navigationItem.hidesBackButton = true
+    navigationController?.isNavigationBarHidden = true
+    UI레이아웃()
+    버튼_클릭()
+}
+
+func UI레이아웃() {
+    view.addSubview(뒤로가기_버튼)
+    view.addSubview(페이지_제목)
+    뒤로가기_버튼.snp.makeConstraints { make in
+        make.centerY.equalTo(페이지_제목.snp.centerY)
+        make.leading.equalToSuperview().offset(16)
+        make.size.equalTo(40)
+    }
+    페이지_제목.snp.makeConstraints { make in
+        make.top.equalToSuperview().offset(60)
+        make.centerX.equalToSuperview()
+    }
+}
+
+    private func 버튼_클릭() {
+        뒤로가기_버튼.addTarget(self, action: #selector(뒤로가기_버튼_클릭), for: .touchUpInside)
+
+    }
+    @objc func 뒤로가기_버튼_클릭() {
+        navigationController?.popViewController(animated: true)
     }
 }
