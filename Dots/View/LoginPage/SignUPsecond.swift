8b2959c8a8a73a5259c4e04024852e4c5e398c62
cheshire0105 <cheshire0105@gmail.com>
2023-11-15 09:36:23 +0900

    [기능 수정] : 회원가입 미술관 선택 컬렉션 뷰 UI 수정 완료 - 코드 정리 이전 https://equaleyes.com/blog/2018/05/10/blog-flow-layout/ - 참고

--- Dots/View/LoginPage/SignUPsecond.swift
+++ Dots/View/LoginPage/SignUPsecond.swift
@@ -14,7 +14,8 @@ class 회원가입_두번째_뷰컨트롤러 : UIViewController, UICollectionVie
          return 셀
      }
 
-    
+    var 선택된셀인덱스: IndexPath?
+
     let 미술관_리스트 : [String] = ["국립 현대 미술관 서울", "백남준 아트센터", "리움 미술관", "호암 미술관", "뮤지엄 산", "서울 시립 미술관", "아르떼 뮤지엄 제주", "국립 현대 미술관 과천", "국립 중앙 박물관", "대림 미술관","예술의 전당","서울미술관","청주시립미술관","포항 시립 미술관","북서울미술관","수원 시립 아트 스페이스 광교", "부산 현대 미술관","석파정 서울 미술관","디뮤지엄", "경기도 미술관","일민 미술관"]
     
     let 뒤로가기_버튼 = {
@@ -82,7 +83,7 @@ class 회원가입_두번째_뷰컨트롤러 : UIViewController, UICollectionVie
     }()
     
     private let 미술관_컬렉션뷰: UICollectionView = {
-        let 레이아웃 = UICollectionViewFlowLayout()
+        let 레이아웃 = CenterAlignedCollectionViewFlowLayout()
         레이아웃.scrollDirection = .vertical
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: 레이아웃)
         collectionView.backgroundColor = .black
@@ -97,6 +98,7 @@ class 회원가입_두번째_뷰컨트롤러 : UIViewController, UICollectionVie
         미술관_컬렉션뷰.delegate = self
         미술관_컬렉션뷰.dataSource = self
         미술관_컬렉션뷰.register(미술관셀.self, forCellWithReuseIdentifier: 미술관셀.identifier)
+        미술관_컬렉션뷰.allowsMultipleSelection = true
 
     }
 }
@@ -200,6 +202,71 @@ extension 회원가입_두번째_뷰컨트롤러: UICollectionViewDelegateFlowLa
     }
 }
 
+extension 회원가입_두번째_뷰컨트롤러 {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // 이미 선택된 셀을 다시 탭하면 선택을 해제합니다.
+        if let previousIndex = 선택된셀인덱스, previousIndex == indexPath {
+            // 선택 해제
+            collectionView.deselectItem(at: indexPath, animated: true)
+            collectionView.delegate?.collectionView?(collectionView, didDeselectItemAt: indexPath)
+            return
+        }
+
+        // 새 셀을 선택합니다.
+        선택된셀인덱스 = indexPath
+
+        // 선택된 셀의 UI를 업데이트합니다.
+        if let 셀 = collectionView.cellForItem(at: indexPath) as? 미술관셀 {
+            셀.업데이트UI(선택됨: true)
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        // 선택 해제된 셀의 UI를 업데이트합니다.
+        if let 셀 = collectionView.cellForItem(at: indexPath) as? 미술관셀 {
+            셀.업데이트UI(선택됨: false)
+        }
+
+        // 선택 해제 상태를 저장합니다.
+        if 선택된셀인덱스 == indexPath {
+            선택된셀인덱스 = nil
+        }
+    }
+}
+
+
+
+class CenterAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let attributesSuper = super.layoutAttributesForElements(in: rect)!
+        let attributes = NSArray(array: attributesSuper, copyItems: true) as! [UICollectionViewLayoutAttributes]
+
+        // 각 줄별로 그룹화된 셀들의 속성을 저장할 딕셔너리입니다.
+        var rowsDictionary = [Int: [UICollectionViewLayoutAttributes]]()
+
+        // 각 셀의 위치를 계산합니다.
+        for attribute in attributes {
+            let rowNumber = Int(floor(attribute.center.y / attribute.frame.height))
+            rowsDictionary[rowNumber, default: []].append(attribute)
+        }
+
+        // 각 줄의 셀들을 중앙 정렬합니다.
+        for (_, rowAttributes) in rowsDictionary {
+            let sortedRowAttributes = rowAttributes.sorted { $0.frame.origin.x < $1.frame.origin.x }
+            let totalRowWidth = sortedRowAttributes.reduce(0) { $0 + $1.frame.width + minimumInteritemSpacing } - minimumInteritemSpacing
+            var offsetX = (collectionView!.bounds.width - totalRowWidth) / 2
+            for attribute in sortedRowAttributes {
+                var frame = attribute.frame
+                frame.origin.x = offsetX
+                attribute.frame = frame
+                offsetX += frame.width + minimumInteritemSpacing
+            }
+        }
+
+        return attributes
+    }
+}
+
 
 
 
@@ -214,7 +281,6 @@ class 미술관셀: UICollectionViewCell {
         contentView.addSubview(라벨)
         라벨.textAlignment = .center
         라벨.textColor = .white
-        라벨.backgroundColor = .black
         라벨.font = UIFont.systemFont(ofSize: 16)
         라벨.snp.makeConstraints { make in
             make.edges.equalToSuperview()
@@ -233,6 +299,12 @@ class 미술관셀: UICollectionViewCell {
     func 구성(미술관이름: String) {
         라벨.text = 미술관이름
     }
+
+    func 업데이트UI(선택됨: Bool) {
+        contentView.backgroundColor = 선택됨 ? UIColor(red: 0.882, green: 1, blue: 0, alpha: 1) : .clear // 라운드 효과가 있는 contentView의 배경색을 바꿉니다.
+        라벨.textColor = 선택됨 ? .black : .white
+    }
+
 }
 
 
