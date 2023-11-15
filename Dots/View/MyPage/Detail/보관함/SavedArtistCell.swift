1570869581ae930eb4e978481a5c2a26c8448cb5
judas <churoo0418@gmail.com>
2023-11-15 17:41:26 +0900

    [마이페이지_브랜드] 마이페이지_보관함 - 전시/리뷰 완료  // 아티스트 + 미술관 커스텀셀 작업중

--- Dots/View/MyPage/Detail/보관함/SavedArtistCell.swift
+++ Dots/View/MyPage/Detail/보관함/SavedArtistCell.swift
@@ -1,8 +1,57 @@
-//
-//  SavedArtistCell.swift
-//  Dots
-//
-//  Created by chul on 11/15/23.
-//
+import UIKit
 
-import Foundation
+class 보관함_아티스트_셀 : UICollectionViewCell {
+    var 보관함_아티스트_이미지 = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "help")
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    } ()
+    
+    let 보관함_아티스트_이름 = {
+        let label = UILabel()
+        label.text = "아티스트 이름"
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "HelveticaNeue", size: 12)
+        label.textAlignment = .left
+        return label
+    } ()
+  
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.orange
+        layer.cornerRadius = 60
+        layer.borderWidth = 0.5
+        layer.borderColor = UIColor(named: "neon")?.cgColor
+        보관함_아티스트_셀_레이아웃()
+        
+    }
+    
+    func 보관함_아티스트_셀_레이아웃() {
+        contentView.addSubview(보관함_아티스트_이미지)
+        contentView.addSubview(보관함_아티스트_이름)
+        
+        보관함_아티스트_이미지.snp.makeConstraints { make in
+            
+        }
+        보관함_아티스트_이름.snp.makeConstraints { make in
+            
+        }
+        
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
