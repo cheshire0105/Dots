1595ebbc8abf925a54fa6a128370e7772c307876
cheshire <blackwinter0105@outlook.kr>
2023-11-20 14:40:01 +0900

    [레이아웃 수정] : 서치바 네비게이션 숨김으로 서치바 올림

--- Dots/View/SearchPage/SearchPage.swift
+++ Dots/View/SearchPage/SearchPage.swift
@@ -47,6 +47,19 @@ class SearchPage: UIViewController, UISearchBarDelegate, UITableViewDelegate, UI
         }
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 네비게이션 바 숨기기
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // 다른 화면으로 넘어갈 때 네비게이션 바 다시 표시
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+
     func setupCollectionViewLayout() {
         guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
         layout.scrollDirection = .vertical
