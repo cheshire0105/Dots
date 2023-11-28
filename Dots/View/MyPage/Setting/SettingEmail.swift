ba6a7db1ecb2bc7aa048d87c738836aff32d7416
judas <churoo0418@gmail.com>
2023-11-28 14:35:44 +0900

    [마이페이지_브랜치] 유저설정- 프로필 변경 페이지 ui 업데이트 + 기능 업데이트 완료 (추후 기능관련 유저 계정과 연동 작업 예정)

--- Dots/View/MyPage/Setting/SettingEmail.swift
+++ Dots/View/MyPage/Setting/SettingEmail.swift
@@ -18,6 +18,56 @@ class 이메일변경_화면 : UIViewController {
         label.textAlignment = .center
         return label
     }()
+    
+    private let 현재_이메일_텍스트필드 = { ()
+        let textField = UITextField()
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.lightGray,
+            .font: UIFont.boldSystemFont(ofSize: 14)
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: "이메일", attributes: attributes)
+        textField.textColor = UIColor.white
+        textField.tintColor = UIColor(named: "neon")
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 25
+        //        textField.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1)
+        textField.backgroundColor = UIColor.clear
+        textField.textAlignment = .left
+        textField.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        textField.keyboardType = .emailAddress
+        textField.autocapitalizationType = .none
+        
+        textField.clearButtonMode = .whileEditing
+        textField.rightViewMode = .whileEditing
+        return textField
+    } ()
+    
+    private let 새_이메일_텍스트필드 = { ()
+        let textField = UITextField()
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.lightGray,
+            .font: UIFont.boldSystemFont(ofSize: 14)
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: "이메일", attributes: attributes)
+        textField.textColor = UIColor.white
+        textField.tintColor = UIColor(named: "neon")
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 25
+        //        textField.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1)
+        textField.backgroundColor = UIColor.clear
+        textField.textAlignment = .left
+        textField.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        textField.keyboardType = .emailAddress
+        textField.autocapitalizationType = .none
+        
+        textField.clearButtonMode = .whileEditing
+        textField.rightViewMode = .whileEditing
+        
+        return textField
+    } ()
+    
     override func viewDidLoad() {
         view.backgroundColor = .black
         view.layer.cornerRadius = 20
@@ -34,6 +84,8 @@ extension 이메일변경_화면{
     private func UI레이아웃 () {
         view.addSubview(뒤로가기_버튼)
         view.addSubview(페이지_제목)
+        view.addSubview(현재_이메일_텍스트필드)
+        view.addSubview(새_이메일_텍스트필드)
         뒤로가기_버튼.snp.makeConstraints { make in
             make.centerY.equalTo(페이지_제목.snp.centerY)
             make.leading.equalToSuperview().offset(16)
@@ -44,7 +96,25 @@ extension 이메일변경_화면{
             make.centerX.equalToSuperview()
             make.height.equalTo(44)
         }
+        현재_이메일_텍스트필드.snp.makeConstraints { make in
+            make.top.equalTo(페이지_제목.snp.bottom).offset(86)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(44)
+        }
+        
+        새_이메일_텍스트필드.snp.makeConstraints { make in
+            make.top.equalTo(현재_이메일_텍스트필드.snp.bottom).offset(85)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(44)
+        }
     }
+}
+
+
+extension 이메일변경_화면 {
+    
     private func 버튼_클릭() {
         뒤로가기_버튼.addTarget(self, action: #selector(뒤로가기_버튼_클릭), for: .touchUpInside)
     }
