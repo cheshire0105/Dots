8772777da1283439045a38a260231338d8758174
cheshire0105 <cheshire0105@gmail.com>
2023-11-28 13:54:11 +0900

    [기능 추가] : 전시 상세 페이지 데이터 바인딩 완료. - 문단 바꾸는거 예정

--- Dots/Model/ExhibitionModel.swift
+++ Dots/Model/ExhibitionModel.swift
@@ -29,6 +29,8 @@ struct ExhibitionDetailModel {
     var exhibitionDates: String // 전시 날짜
     var exhibitionHours: String // 전시 시간
     var exhibitionTitle: String // 전시 타이틀
+    var exhibitionGeo : String // 전시 좌표
+    var exhibitionDetail : String // 전시 설명
 
     init(dictionary: [String: Any]) {
         self.museumName = dictionary["미술관_이름"] as? String ?? "Unknown Title"
@@ -39,6 +41,8 @@ struct ExhibitionDetailModel {
         self.exhibitionDates = dictionary["전시_날짜"] as? String ?? "Unknown Title"
         self.exhibitionHours = dictionary["전시_시간"] as? String ?? "Unknown Title"
         self.exhibitionTitle = dictionary["전시_타이틀"] as? String ?? "Unknown Title"
+        self.exhibitionGeo = dictionary["전시_좌표"] as? String ?? "Unknown Title"
+        self.exhibitionDetail = dictionary["전시_설명"] as? String ?? "Unknown Title"
     }
 }
 
