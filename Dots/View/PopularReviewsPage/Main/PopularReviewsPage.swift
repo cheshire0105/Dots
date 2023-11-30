bc1fa0856a1ee5d9919e681e7619170fd5e4ae07
cheshire0105 <cheshire0105@gmail.com>
2023-11-30 09:05:44 +0900

    [내부 수정] : 임시로 데이터 넣은 것들을 바인딩 할 수 있게 배열로 수정, 라이트 모드로 강제 해서 이상한 선 없엠.

--- Dots/View/PopularReviewsPage/Main/PopularReviewsPage.swift
+++ Dots/View/PopularReviewsPage/Main/PopularReviewsPage.swift
@@ -12,6 +12,10 @@ class PopularReviewsPage: UIViewController, UITableViewDataSource, UITableViewDe
         setupTableView()
         setupNavigationBar()
         view.backgroundColor = .black
+//        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: "TweetCell")
+
+        // 라이트 모드로 강제 설정
+            overrideUserInterfaceStyle = .light
 
     }
 
@@ -22,13 +26,15 @@ class PopularReviewsPage: UIViewController, UITableViewDataSource, UITableViewDe
         tableView.backgroundColor = .black
 
         // UITableViewCell.self 대신 TweetTableViewCell.self로 변경해야 합니다.
-        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: "TweetCell")
+        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: "TweetTableViewCell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
 
         // Constraints 설정
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
+
+
         }
     }
     func setupNavigationBar() {
@@ -57,12 +63,29 @@ class PopularReviewsPage: UIViewController, UITableViewDataSource, UITableViewDe
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 커스텀 셀로 구성
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetTableViewCell
-        // 셀에 임시 데이터를 바인딩
-        cell.configure()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetTableViewCell", for: indexPath) as! TweetTableViewCell
+
+        cell.backgroundColor = .black
+
+        let tweetData = TweetData(
+            profileImageName: "cabanel",
+            userName: "cheshire",
+            exhibitionName: "서울시립미술관",
+            tweetImageName: "morningStar",
+            tweetTitle: "전시 제목 입니다.",
+            tweetContent: """
+            오늘 전시회를 방문해서 정말 멋진 시간을 보냈습니다. 작품들은 아름답고 감동적이었고, 예술가들의 역량과 창의성에 감탄했습니다. 특히, 회화와 조각 작품들은 각기 다른 스타일과 표현력을 가지고 있어서 볼 때마다 새로운 감정을 느끼게 해주었습니다. 전시 공간 자체도 아주 아름다웠는데, 조명과 배치가 조화롭게 어우러져 작품들을 더욱 빛나게 했습니다. 또한, 전시 관람 도중 아티스트와 대화할 기회가 있어서 그들의 작업에 대한 인사이트를 얻을 수 있어서 특별한 경험이었습니다. 전시 후기를 쓰며, 예술의 아름다움과 힘을 다시 한 번 느낄 수 있었습니다. 이런 멋진 전시회를 기획하고 준비한 모든 분들에게 감사의 인사를 전하고 싶습니다. 더 많은 사람들이 이런 아름다운 예술을 만끽할 수 있기를 바랍니다.
+            """,
+            timeAgo: "8분전",
+            additionalLabel1Text: "123",
+            additionalLabel2Text: "456"
+        )
+
+        cell.configure(with: tweetData)
         return cell
     }
+
+
 
     // UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -107,6 +130,8 @@ class TweetTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         // 컴포넌트 설정 및 레이아웃 구성
+
+
         addSubviews()
         setupLayout()
 
@@ -198,60 +223,71 @@ class TweetTableViewCell: UITableViewCell {
     }
 
     // 임시 데이터 바인딩을 위한 메서드
-    func configure() {
+    func configure(with data: TweetData) {
         // 배경색 설정
         self.backgroundColor = UIColor.black
 
         // 프로필 이미지 설정 (이미지 이름은 프로젝트에 있는 이미지명과 일치해야 합니다)
-        profileImageView.image = UIImage(named: "cabanel")
+//        profileImageView.image = UIImage(named: "cabanel")
+        profileImageView.image = UIImage(named: data.profileImageName)
+
         profileImageView.layer.cornerRadius = 25
         profileImageView.clipsToBounds = true
 
         // 유저 닉네임 레이블 설정
-        nameLabel.text = "cheshire"
+//        nameLabel.text = "cheshire"
+        nameLabel.text = data.userName
+
         nameLabel.textColor = UIColor.white
         nameLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold) // 폰트 크기와 두께 설정
 
         // 전시회 정보 레이블 설정
-        exhibitionInfoLabel.text = "국립현대미술관"
+//        exhibitionInfoLabel.text = "국립현대미술관"
+        exhibitionInfoLabel.text = data.exhibitionName
+
         exhibitionInfoLabel.textColor = UIColor.white
         exhibitionInfoLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular) // 폰트 크기와 두께 설정
 
         // 피드 이미지 설정 (이미지 이름은 프로젝트에 있는 이미지명과 일치해야 합니다)
-        tweetImageView.image = UIImage(named: "morningStar")
+//        tweetImageView.image = UIImage(named: "morningStar")
+        tweetImageView.image = UIImage(named: data.tweetImageName)
 
-        // 트윗 내용 레이블 설정
-        let originalText = """
-            오늘 전시회를 방문해서 정말 멋진 시간을 보냈습니다. 작품들은 아름답고 감동적이었고, 예술가들의 역량과 창의성에 감탄했습니다. 특히, 회화와 조각 작품들은 각기 다른 스타일과 표현력을 가지고 있어서 볼 때마다 새로운 감정을 느끼게 해주었습니다. 전시 공간 자체도 아주 아름다웠는데, 조명과 배치가 조화롭게 어우러져 작품들을 더욱 빛나게 했습니다. 또한, 전시 관람 도중 아티스트와 대화할 기회가 있어서 그들의 작업에 대한 인사이트를 얻을 수 있어서 특별한 경험이었습니다. 전시 후기를 쓰며, 예술의 아름다움과 힘을 다시 한 번 느낄 수 있었습니다. 이런 멋진 전시회를 기획하고 준비한 모든 분들에게 감사의 인사를 전하고 싶습니다. 더 많은 사람들이 이런 아름다운 예술을 만끽할 수 있기를 바랍니다.
-            """
+        let tweetContent = data.tweetContent // 데이터에서 트윗 내용을 가져옵니다.
 
+        
         let moreText = "더보기..."
+            let attributedString = NSMutableAttributedString(string: tweetContent)
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 14, weight: .regular),
+                .foregroundColor: UIColor.white
+            ]
 
-        let attributedString = NSMutableAttributedString(string: originalText)
-        let attributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 14, weight: .regular),
-            .foregroundColor: UIColor.white
-        ]
+            attributedString.addAttributes(attributes, range: NSRange(location: 0, length: attributedString.length))
 
-        attributedString.addAttributes(attributes, range: NSRange(location: 0, length: attributedString.length))
+            if attributedString.string.count > 100 { // 100자를 초과하는 경우에만 "더보기..." 추가
+                attributedString.mutableString.setString(String(attributedString.string.prefix(100)) + "..." + moreText)
+            }
 
-        if attributedString.string.count > 100 { // 100자를 초과하는 경우에만 "더보기..." 추가
-            attributedString.mutableString.setString(String(attributedString.string.prefix(100)) + "..." + moreText)
-        }
+            tweetContentLabel.attributedText = attributedString
+            tweetContentLabel.numberOfLines = 3
+            tweetContentLabel.font = UIFont(name: "Pretendard-Regular", size: 13)
+            tweetContentLabel.textColor = UIColor(red: 0.733, green: 0.733, blue: 0.733, alpha: 1)
 
-        tweetContentLabel.attributedText = attributedString
-        tweetContentLabel.numberOfLines = 3
-        tweetContentLabel.font = UIFont(name: "Pretendard-Regular", size: 13)
-        tweetContentLabel.textColor = UIColor(red: 0.733, green: 0.733, blue: 0.733, alpha: 1)
+
+
 
 
 
         // "8분 전" 레이블 설정
         timeLabel.text = "8분 전"
+        timeLabel.text = data.timeAgo
+
         timeLabel.textColor = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
         timeLabel.font = UIFont(name: "Pretendard-Light", size: 14)
 
-        titelLabel.text = "전시 후기 제목"
+//        titelLabel.text = "전시 후기 제목"
+        titelLabel.text = data.tweetTitle
+
         titelLabel.textColor = .white
         titelLabel.font = UIFont(name: "Pretendard-Medium", size: 18)
 
@@ -259,7 +295,9 @@ class TweetTableViewCell: UITableViewCell {
         // 첫 번째 추가 이미지 뷰 및 레이블 구성
         additionalImageView1.image = UIImage(named: "Vector 3")
 
-        additionalLabel1.text = "123"
+//        additionalLabel1.text = "123"
+        additionalLabel1.text = data.additionalLabel1Text
+
         additionalLabel1.textColor = .white
         additionalLabel1.font = UIFont(name: "Pretendard-Light", size: 12)
         additionalLabel1.textColor = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
@@ -272,7 +310,9 @@ class TweetTableViewCell: UITableViewCell {
         additionalLabel2.textColor = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
 
 
-        additionalLabel2.text = "456"
+//        additionalLabel2.text = "456"
+        additionalLabel2.text = data.additionalLabel2Text
+
         // 스타일 및 글꼴 설정 등
 
 
@@ -333,4 +373,17 @@ class TweetTableViewCell: UITableViewCell {
     }
 
 
+}
+
+
+struct TweetData {
+    var profileImageName: String
+    var userName: String
+    var exhibitionName: String
+    var tweetImageName: String
+    var tweetTitle: String
+    var tweetContent: String
+    var timeAgo: String
+    var additionalLabel1Text: String
+    var additionalLabel2Text: String
 }
