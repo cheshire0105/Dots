28dc0cfb537c2979a9820a6c5468cca104872e5e
judas <churoo0418@gmail.com>
2023-12-04 03:58:31 +0900

    [마이페이지_브랜치] 유저설정- 비밀번호 변경 관련 몇번씩 오류 발생 이후 정상적으로 함수+서버 작동 연동이 재대로 되지않는 부분 코드 수정 추가 + 테스트 수차례 확인후 업데이트 완료

--- Dots/View/MyPage/Setting/SettingPassword.swift
+++ Dots/View/MyPage/Setting/SettingPassword.swift
@@ -252,81 +252,139 @@ class 비밀번호변경_화면 : UIViewController {
         NotificationCenter.default.removeObserver(self)
     }
 }
-    extension 비밀번호변경_화면 {
+extension 비밀번호변경_화면 {
+    
+    
+    private func 버튼_클릭() {
+        뒤로가기_버튼.addTarget(self, action: #selector(뒤로가기_버튼_클릭), for: .touchUpInside)
+        현재_비밀번호_표시_온오프.addTarget(self, action: #selector(현재_비밀번호_표시_온오프_클릭), for: .touchUpInside)
+        새_비밀번호_표시_온오프.addTarget(self, action: #selector(새_비밀번호_표시_온오프_클릭), for: .touchUpInside)
+        새_비밀번호_확인_표시_온오프.addTarget(self, action: #selector(새_비밀번호_확인_표시_온오프_클릭), for: .touchUpInside)
+        변경_버튼.addTarget(self, action: #selector(변경_버튼_클릭), for: .touchUpInside)
         
-        
-        private func 버튼_클릭() {
-            뒤로가기_버튼.addTarget(self, action: #selector(뒤로가기_버튼_클릭), for: .touchUpInside)
-            현재_비밀번호_표시_온오프.addTarget(self, action: #selector(현재_비밀번호_표시_온오프_클릭), for: .touchUpInside)
-            새_비밀번호_표시_온오프.addTarget(self, action: #selector(새_비밀번호_표시_온오프_클릭), for: .touchUpInside)
-            새_비밀번호_확인_표시_온오프.addTarget(self, action: #selector(새_비밀번호_확인_표시_온오프_클릭), for: .touchUpInside)
-            변경_버튼.addTarget(self, action: #selector(변경_버튼_클릭), for: .touchUpInside)
-
+    }
+    @objc private func 뒤로가기_버튼_클릭() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func 변경_버튼_클릭() {
+        비밀번호변경_완료_업로드_재로그인_실행_함수()
+    }
+    
+    func 비밀번호변경_완료_업로드_재로그인_실행_함수(){
+        guard let 새비밀번호 = 새_비밀번호_텍스트필드.text, !새비밀번호.isEmpty,
+              let 새비밀번호확인 = 새_비밀번호_확인_텍스트필드.text, !새비밀번호확인.isEmpty else {
+            확인알럿(message: "새 비밀번호와 확인 비밀번호를 모두 입력해주세요.")
+            return
         }
-        @objc private func 뒤로가기_버튼_클릭() {
-            navigationController?.popViewController(animated: true)
+        
+        if 새비밀번호 != 새비밀번호확인 {
+            확인알럿(message: "새 비밀번호와 확인 비밀번호가 일치하지 않습니다.")
+            return
         }
         
-        @objc private func 변경_버튼_클릭() {
-               guard let 새비밀번호 = 새_비밀번호_텍스트필드.text, !새비밀번호.isEmpty,
-                     let 새비밀번호확인 = 새_비밀번호_확인_텍스트필드.text, !새비밀번호확인.isEmpty else {
-                   확인알럿(message: "새 비밀번호와 확인 비밀번호를 모두 입력해주세요.")
-                   return
-               }
-
-               if 새비밀번호 != 새비밀번호확인 {
-                   확인알럿(message: "새 비밀번호와 확인 비밀번호가 일치하지 않습니다.")
-                   return
-               }
-
-               if let 유저 = Auth.auth().currentUser {
-                   for auth에등록된계정 in 유저.providerData {
-                       let 제공업체 = auth에등록된계정.providerID
-                       if 제공업체 == "password" {
-                           유저.updatePassword(to: 새비밀번호) { [weak self] (error) in
-                               if let error = error {
-                                   self?.확인알럿(message: "비밀번호 업데이트 실패: \(error.localizedDescription)")
-                               } else {
-                                   self?.확인알럿(message: "비밀번호가 성공적으로 변경되었습니다.")
-                               }
-                           }
-
-                           let 이메일 = 유저.email
-                           if let 이메일 = 이메일 {
-                               let 데이터베이스 = Firestore.firestore()
-                               let 유저데이터조회 = 데이터베이스.collection("도트_유저_데이터_관리").whereField("이메일", isEqualTo: 이메일)
-
-                               유저데이터조회.getDocuments { [weak self] (snapshot, error) in
-                                   guard let self = self, let 문서 = snapshot?.documents.first else {
-                                       return
-                                   }
-
-                                   if 문서.exists {
-                                       let 문서참조 = 데이터베이스.collection("도트_유저_데이터_관리").document(문서.documentID)
-                                       문서참조.updateData(["비밀번호": 새비밀번호]) { error in
-                                           if let error = error {
-                                               self.확인알럿(message: "파이어스토어 비밀번호 업데이트 실패: \(error.localizedDescription)")
-                                           } else {
-                                               print("파이어스토어 비밀번호가 성공적으로 변경되었습니다.")
-                                           }
-                                       }
-                                   } else {
-                                       print("유저 데이터를 찾을 수 없음")
-                                   }
-                               }
-                           } else {
-                               print("조회 결과 등록된 이메일이 없음")
-                           }
-                       } else {
-                           확인알럿(message: "현재는 비밀번호 연동 계정만 변경이 가능합니다.")
-                       }
-                   }
-               } else {
-                   확인알럿(message: "접속중인 계정이 없습니다.")
-               }
-           }
-       
-        
+        if let 유저 = Auth.auth().currentUser {
+            for auth에등록된계정 in 유저.providerData {
+                let 제공업체 = auth에등록된계정.providerID
+                if 제공업체 == "password" {
+                    유저.updatePassword(to: 새비밀번호) { [weak self] (error) in
+                        if let error = error {
+                            self?.확인알럿(message: "비밀번호 업데이트 실패: \(error.localizedDescription)")
+                        } else {
+                            self?.확인알럿(message: "비밀번호가 성공적으로 변경되었습니다.")
+                            self?.새비밀번호_파이어스토어_업로드 (새비밀번호)
+                        }
+                    }
+                } else {
+                    확인알럿(message: "도트회원가입 계정만 변경이 가능합니다.")
+                }
+            }
+        } else {
+            확인알럿(message: "접속중인 계정이 없습니다.")
+        }
+    }
+    
+    func 새비밀번호_파이어스토어_업로드 (_ 새비밀번호: String) {
+            guard let 유저 = Auth.auth().currentUser, let 이메일 = 유저.email else {
+                return
+            }
+            let 데이터베이스 = Firestore.firestore()
+            let 유저데이터조회 = 데이터베이스.collection("도트_유저_데이터_관리").whereField("이메일", isEqualTo: 이메일)
+            
+            유저데이터조회.getDocuments { [weak self] (snapshot, error) in
+                guard let self = self, let 문서 = snapshot?.documents.first else {
+                    return
+                }
+                
+                if 문서.exists {
+                    let 문서참조 = 데이터베이스.collection("도트_유저_데이터_관리").document(문서.documentID)
+                    문서참조.updateData(["비밀번호": 새비밀번호]) { error in
+                        if let error = error {
+                            self.확인알럿(message: "파이어스토어 비밀번호 업데이트 실패: \(error.localizedDescription)")
+                        } else {
+                            print("파이어스토어 비밀번호가 성공적으로 변경되었습니다.")
+                            self.새비밀번호로_재접속시키기()
+                        }
+                    }
+                } else {
+                    print("유저 데이터를 찾을 수 없음")
+                }
+            }
+        }
+    func 새비밀번호로_재접속시키기() {
+        do {
+            if let 현재접속중인유저 = Auth.auth().currentUser {
+                print("로그아웃한 사용자 정보:")
+                print("UID: \(현재접속중인유저.uid)")
+                print("이메일: \(현재접속중인유저.email ?? "없음")")
+                
+                let 파이어스토어 = Firestore.firestore()
+                let 이메일 = 현재접속중인유저.email ?? ""
+                let 유저컬렉션: CollectionReference
+                
+                if let providerID = 현재접속중인유저.providerData.first?.providerID, providerID == GoogleAuthProviderID {
+                    유저컬렉션 = 파이어스토어.collection("구글_유저_데이터_관리")
+                } else {
+                    유저컬렉션 = 파이어스토어.collection("도트_유저_데이터_관리")
+                }
+                
+                유저컬렉션.whereField("이메일", isEqualTo: 이메일).getDocuments { [weak self] (querySnapshot, 에러) in
+                    guard let self = self else { return }
+                    
+                    if let 에러 = 에러 {
+                        print("Firestore 조회 에러: \(에러.localizedDescription)")
+                    } else {
+                        if let 문서조회 = querySnapshot?.documents, !문서조회.isEmpty {
+                            let 문서 = 문서조회[0]
+                            let 현재날짜시간 = Timestamp(date: Date())
+                            
+                            문서.reference.updateData(["로그인상태": false, "마지막로그아웃": 현재날짜시간]) { (에러) in
+                                if let 에러 = 에러 {
+                                    print("Firestore 업데이트 에러: \(에러.localizedDescription)")
+                                } else {
+                                    print("Firestore: 로그인 상태 : false")
+                                }
+                            }
+                        } else {
+                            print("Firestore: 일치하는 이메일이 없습니다.")
+                        }
+                    }
+                }
+            }
+                try Auth.auth().signOut()
+                print("계정이 로그아웃되었습니다.")
+                        let 로그인_뷰컨트롤러 = 로그인_뷰컨트롤러()
+                        let 로그인화면_이동 = UINavigationController(rootViewController: 로그인_뷰컨트롤러)
+                        로그인화면_이동.modalPresentationStyle = .fullScreen
+            dismiss(animated: true) {
+                UserDefaults.standard.removeObject(forKey: "isUserLoggedIn")
+                self.present(로그인화면_이동, animated: true, completion: nil)
+            }
+            } catch {
+            print("로그아웃 실패: \(error.localizedDescription)")
+        }
+    }
+    
     @objc func 현재_비밀번호_표시_온오프_클릭() {
         if 현재_비밀번호_텍스트필드.isSecureTextEntry == true {
             현재_비밀번호_표시_온오프.setImage(UIImage(systemName: "eye"), for: .normal)
@@ -348,15 +406,15 @@ class 비밀번호변경_화면 : UIViewController {
         }
     }
     @objc func 새_비밀번호_확인_표시_온오프_클릭() {
-            if 새_비밀번호_확인_텍스트필드.isSecureTextEntry == true {
-                새_비밀번호_확인_표시_온오프.setImage(UIImage(systemName: "eye"), for: .normal)
-                새_비밀번호_확인_표시_온오프.tintColor = UIColor(named: "neon")
-                새_비밀번호_확인_텍스트필드.isSecureTextEntry = false
-            } else if 새_비밀번호_확인_텍스트필드.isSecureTextEntry == false {
-                새_비밀번호_확인_표시_온오프.setImage(UIImage(named: "passwordOFF"), for: .normal)
-                새_비밀번호_확인_텍스트필드.isSecureTextEntry = true
-            }
+        if 새_비밀번호_확인_텍스트필드.isSecureTextEntry == true {
+            새_비밀번호_확인_표시_온오프.setImage(UIImage(systemName: "eye"), for: .normal)
+            새_비밀번호_확인_표시_온오프.tintColor = UIColor(named: "neon")
+            새_비밀번호_확인_텍스트필드.isSecureTextEntry = false
+        } else if 새_비밀번호_확인_텍스트필드.isSecureTextEntry == false {
+            새_비밀번호_확인_표시_온오프.setImage(UIImage(named: "passwordOFF"), for: .normal)
+            새_비밀번호_확인_텍스트필드.isSecureTextEntry = true
         }
+    }
 }
 
 
@@ -504,7 +562,7 @@ extension 비밀번호변경_화면 {
                     현재_비밀번호_텍스트필드.isEnabled = false
                     현재_비밀번호_텍스트필드.isSecureTextEntry = false
                     현재_비밀번호_표시_온오프.isHidden = true
-
+                    
                     새_비밀번호_텍스트필드.text = "구글 연동 로그인의 경우 사용 변경 불가"
                     새_비밀번호_텍스트필드.textColor = UIColor.red
                     새_비밀번호_텍스트필드.isEnabled = false
@@ -516,7 +574,7 @@ extension 비밀번호변경_화면 {
                     새_비밀번호_확인_텍스트필드.isEnabled = false
                     새_비밀번호_확인_텍스트필드.isSecureTextEntry = false
                     새_비밀번호_확인_표시_온오프.isHidden = true
-
+                    
                 }
             }
         } else {
