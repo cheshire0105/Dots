3c732ec098be4757dbc5565649d519c4b681e344
cheshire0105 <cheshire0105@gmail.com>
2023-12-04 19:42:00 +0900

    [기능 추가] : 가입 될 때 닉네임과 프로필 아이디를 서버에 저장 - 후기 셀들에서 사용을 위해

--- Dots/View/LoginPage/SNS/Google.swift
+++ Dots/View/LoginPage/SNS/Google.swift
@@ -7,11 +7,11 @@ import Firebase
 import Foundation
 
 extension 로그인_뷰컨트롤러 {
-    
-    
+
+
     @objc func 구글_버튼_클릭() {
         print("구글 페이지로 연결")
-        
+
         GIDSignIn.sharedInstance.signIn(withPresenting: self) { authentication, 에러 in
             if let 에러 = 에러 {
                 print("There is an error signing the user in ==> \(에러)")
@@ -20,34 +20,48 @@ extension 로그인_뷰컨트롤러 {
             guard let 구글계정 = authentication?.user, let idToken = 구글계정.idToken?.tokenString else { return }
             //앱체크 토큰가져오기 아직은 콘솔창에서 버전 파이어베이스 앱체크 api 부재로 못가져온다고나옴
             let 토큰가져오기 = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: 구글계정.accessToken.tokenString)
-            
-                
-                    Auth.auth().signIn(with: 토큰가져오기) { (authResult, 에러) in
-                        if let 에러 = 에러 {
-                            print(에러.localizedDescription)
-                            print("토큰가져오기 에러")
-                        } else {
-                            print("구글 로그인 성공")
-                            
-                            let 구글이메일 = 구글계정.profile?.email ?? "default email"
-                            let 구글닉네임 = 구글계정.profile?.name ?? "default name"
-                            let 식별id = self.아이디_추출(구글이메일) // 구글은 email로부터 id 생성
-                            let 프로필이미지URL = 구글계정.profile?.imageURL(withDimension: 200)?.absoluteString ?? "default image URL"
-                            
-                            self.구글계정정보_파이어스토어_업로드(회원가입_타입: "구글", 구글이메일: 구글이메일, 구글닉네임: 구글닉네임, 식별id: 식별id, 로그인상태: true, 프로필이미지URL: 프로필이미지URL)
-                            
-                            UserDefaults.standard.set(true, forKey: "isUserLoggedIn")
-                            
-                            let 메인화면_이동 = TabBar()
-                            self.navigationController?.pushViewController(메인화면_이동, animated: true)
-                            self.navigationItem.hidesBackButton = true
-                        }
-                    }
+
+
+            Auth.auth().signIn(with: 토큰가져오기) { (authResult, 에러) in
+                if let 에러 = 에러 {
+                    print(에러.localizedDescription)
+                    print("토큰가져오기 에러")
+                } else {
+                    print("구글 로그인 성공")
+
+                    guard let 구글계정 = authResult?.user else { return }
+                    let 구글이메일 = 구글계정.email ?? "default email"
+                    let 구글닉네임 = 구글계정.displayName ?? "default name"
+                    let 프로필이미지URL = 구글계정.photoURL?.absoluteString ?? "default image URL"
+                    let 유저UID = 구글계정.uid
+                    let 식별id = self.아이디_추출(구글이메일) // 구글은 email로부터 id 생성
+
+
+                    // Firestore에 사용자 정보 저장 또는 업데이트
+                    self.구글계정정보_파이어스토어_업로드(
+                        회원가입_타입: "구글",
+                        구글이메일: 구글이메일,
+                        구글닉네임: 구글닉네임,
+                        식별id: 식별id, // 여기에 식별id 값을 제공해야 합니다.
+                        로그인상태: true,
+                        프로필이미지URL: 프로필이미지URL,
+                        유저UID: 유저UID
+                    )
+
+
+                    UserDefaults.standard.set(true, forKey: "isUserLoggedIn")
+
+                    let 메인화면_이동 = TabBar()
+                    self.navigationController?.pushViewController(메인화면_이동, animated: true)
+                    self.navigationItem.hidesBackButton = true
                 }
             }
-        
-    
-    
+
+        }
+    }
+
+
+
     func 아이디_추출(_ email: String) -> String {
         let 이메일에서ID추출 = email.components(separatedBy: "@")
         guard 이메일에서ID추출.count == 2 else {
@@ -57,10 +71,10 @@ extension 로그인_뷰컨트롤러 {
         print("구글 계정 UID 생성 완료 : 파이어 스토어 문서화를 위함")
         return 이메일에서ID추출[0]
     }
-    
-    
-    
-    func 구글계정정보_파이어스토어_업로드(회원가입_타입: String,구글이메일: String, 구글닉네임: String, 식별id: String,로그인상태: Bool, 프로필이미지URL: String) {
+
+
+
+    func 구글계정정보_파이어스토어_업로드(회원가입_타입: String, 구글이메일: String, 구글닉네임: String, 식별id: String, 로그인상태: Bool, 프로필이미지URL: String, 유저UID: String) {
         let 데이터베이스 = Firestore.firestore()
         let 유저컬렉션 = 데이터베이스.collection("구글_유저_데이터_관리").document(식별id)
         let 현재날짜시간 = Timestamp(date: Date())
@@ -73,7 +87,7 @@ extension 로그인_뷰컨트롤러 {
             "마지막로그인": 현재날짜시간,
             "마지막로그아웃": "로그아웃 기록이 없음"
         ]
-        
+
         유저컬렉션.setData(userData) { 에러 in
             if let 에러 = 에러 {
                 print("Firestore 구글 데이터 등록 실패: \(에러.localizedDescription)")
@@ -81,14 +95,31 @@ extension 로그인_뷰컨트롤러 {
                 print("구글 계정 auth 등록후 필요 데이터를 Firestore에 구글 데이터 등록 성공")
             }
         }
+
+        // 새로운 컬렉션에 저장할 데이터 설정
+        let 새로운userData: [String: Any] = [
+            "닉네임": 구글닉네임,
+            "프로필이미지URL": 프로필이미지URL,
+            "UUID": 유저UID  // UUID 필드 추가
+        ]
+
+        // 새로운 컬렉션에 데이터 저장
+        let 새로운유저컬렉션 = 데이터베이스.collection("유저_프로필").document(유저UID) // UUID를 문서 ID로 사용
+        새로운유저컬렉션.setData(새로운userData) { 에러 in
+            if let 에러 = 에러 {
+                print("새로운 컬렉션에 데이터 등록 실패: \(에러.localizedDescription)")
+            } else {
+                print("새로운 컬렉션에 데이터 등록 성공")
+            }
+        }
     }
 }
 
 extension 회원가입_첫번째_뷰컨트롤러 {
-    
+
     @objc func 구글_버튼_클릭() {
         print("구글 페이지로 연결")
-        
+
         GIDSignIn.sharedInstance.signIn(withPresenting: self) { authentication, 에러 in
             if let 에러 = 에러 {
                 print("There is an error signing the user in ==> \(에러)")
@@ -97,21 +128,21 @@ extension 회원가입_첫번째_뷰컨트롤러 {
             guard let 구글계정 = authentication?.user, let idToken = 구글계정.idToken?.tokenString else { return }
             //앱체크 토큰가져오기 아직은 콘솔창에서 버전 파이어베이스 앱체크 api 부재로 못가져온다고나옴
             let 토큰가져오기 = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: 구글계정.accessToken.tokenString)
-            
+
             Auth.auth().signIn(with: 토큰가져오기) { (authResult, 에러) in
                 if let 에러 = 에러 {
                     print(에러.localizedDescription)
                     print("토큰가져오기 에러")
                 } else {
                     print("구글 로그인 성공")
-                    
+
                     let 구글이메일 = 구글계정.profile?.email ?? "default email"
                     let 구글닉네임 = 구글계정.profile?.name ?? "default name"
                     let 식별id = self.아이디_추출(구글이메일) // 구글은 email로부터 id 생성
                     let 프로필이미지URL = 구글계정.profile?.imageURL(withDimension: 200)?.absoluteString ?? "default image URL"
-                    
+
                     self.구글계정정보_파이어스토어_업로드(회원가입_타입: "구글", 구글이메일: 구글이메일, 구글닉네임: 구글닉네임, 식별id: 식별id, 로그인상태: true, 프로필이미지URL: 프로필이미지URL)
-                    
+
                     let 메인화면_이동 = TabBar()
                     self.navigationController?.pushViewController(메인화면_이동, animated: true)
                     self.navigationItem.hidesBackButton = true
@@ -119,8 +150,8 @@ extension 회원가입_첫번째_뷰컨트롤러 {
             }
         }
     }
-    
-    
+
+
     func 아이디_추출(_ email: String) -> String {
         let 이메일에서ID추출 = email.components(separatedBy: "@")
         guard 이메일에서ID추출.count == 2 else {
@@ -130,9 +161,9 @@ extension 회원가입_첫번째_뷰컨트롤러 {
         print("구글 계정 UID 생성 완료 : 파이어 스토어 문서화를 위함")
         return 이메일에서ID추출[0]
     }
-    
-    
-    
+
+
+
     func 구글계정정보_파이어스토어_업로드(회원가입_타입: String,구글이메일: String, 구글닉네임: String, 식별id: String,로그인상태: Bool, 프로필이미지URL: String) {
         let 데이터베이스 = Firestore.firestore()
         let 유저컬렉션 = 데이터베이스.collection("구글_유저_데이터_관리").document(식별id)
@@ -148,7 +179,7 @@ extension 회원가입_첫번째_뷰컨트롤러 {
             "마지막로그아웃": "로그아웃 기록이 없음"
 
         ]
-        
+
         유저컬렉션.setData(userData) { 에러 in
             if let 에러 = 에러 {
                 print("Firestore 구글 데이터 등록 실패: \(에러.localizedDescription)")
