e73842eab7397b192fc4d61c4f2141d2bf638d2d
judas <churoo0418@gmail.com>
2023-12-10 05:41:42 +0900

    [마이페이지_브랜치] 켈린더 - 특정 날짜에 등록된 이미지가   페이징 (월 바꿈) 이루어질때 셀이 재사용되면서 랜덤하게 갯수+지정날짜가 아닌 랜덤 날짜로 뒤섞이는 부분 업데이트

--- Dots/View/MyPage/Calendar/CalendarDelegateExtension.swift
+++ Dots/View/MyPage/Calendar/CalendarDelegateExtension.swift
@@ -15,41 +15,44 @@ extension Mypage: FSCalendarDelegate, FSCalendarDataSource {
         return 0
     }
 
- 
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+          
+           calendar.reloadData()
+       }
 
     func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
-        if specificDates.contains { Calendar.current.isDate(date, inSameDayAs: $0) } {
-            return nil // 이미지를 직접 추가할 것이므로 nil 반환
+        if specificDates.contains(where: { Calendar.current.isDate(date, inSameDayAs: $0) }) {
+            return nil
         }
 
         return nil
     }
 
-    func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
-        let cell = calendar.dequeueReusableCell(withIdentifier: "cell", for: date, at: position)
+    func calendar(_ calendar: FSCalendar, cellFor date: Date, at 특정_날짜셀에_이미지_넣기: FSCalendarMonthPosition) -> FSCalendarCell {
+        let cell = calendar.dequeueReusableCell(withIdentifier: "cell", for: date, at: 특정_날짜셀에_이미지_넣기)
 
-        if position == .current, let cell = cell as? FSCalendarCell, specificDates.contains { Calendar.current.isDate(date, inSameDayAs: $0) } {
-            // 이미지 셀이 존재하지 않을 때에만 추가
+        if 특정_날짜셀에_이미지_넣기 == .current, let cell = cell as? FSCalendarCell, specificDates.contains(where: { Calendar.current.isDate(date, inSameDayAs: $0) }) {
+
+
             if cell.subviews.filter({ $0 is UIButton }).isEmpty {
-                let imageCell = UIButton(type: .custom)
-                imageCell.setImage(UIImage(named: "help"), for: .normal)
-                imageCell.layer.borderWidth = 1
-                imageCell.layer.borderColor = UIColor(named: "neon")?.cgColor
-                imageCell.clipsToBounds = true
-                imageCell.layer.masksToBounds = true
-                imageCell.layer.cornerRadius = 20
-                imageCell.isUserInteractionEnabled = false
+                let 날짜에_등록할_이미지 = UIButton(type: .custom)
+                날짜에_등록할_이미지.setImage(UIImage(named: "help"), for: .normal)
+                날짜에_등록할_이미지.layer.borderWidth = 1
+                날짜에_등록할_이미지.layer.borderColor = UIColor(named: "neon")?.cgColor
+                날짜에_등록할_이미지.clipsToBounds = true
+                날짜에_등록할_이미지.layer.masksToBounds = true
+                날짜에_등록할_이미지.layer.cornerRadius = 20
+                날짜에_등록할_이미지.isUserInteractionEnabled = false
 
-                let imageSize = CGSize(width: cell.frame.width, height: cell.frame.height)
-
-                // 이미지를 셀 중앙에 위치시키기
-                cell.addSubview(imageCell)
-                imageCell.snp.makeConstraints { make in
+                cell.addSubview(날짜에_등록할_이미지)
+                날짜에_등록할_이미지.snp.makeConstraints { make in
                     make.centerX.equalTo(cell.snp.centerX)
                     make.centerY.equalTo(cell.snp.centerY).offset(-5)
                     make.size.equalTo(40)
                 }
             }
+        } else {
+            cell.subviews.filter { $0 is UIButton }.forEach { $0.removeFromSuperview() }
         }
 
         return cell
@@ -71,7 +74,6 @@ extension Mypage: FSCalendarDelegate, FSCalendarDataSource {
 
 
 //켈린더 sheetpresent modal
-
 extension Mypage {
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
             if Calendar.current.isDateInToday(date) {
