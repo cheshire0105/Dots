b103ba6d2d1023a8ac802608ac5fe5765518ffb2
cheshire0105 <cheshire0105@gmail.com>
2023-12-10 15:05:14 +0900

    [기능 수정] : 검색 페이지 텍스트 캐싱으로 리소스 절약

--- Dots/View/SearchPage/searchPageTableViewCell.swift
+++ Dots/View/SearchPage/searchPageTableViewCell.swift
@@ -18,12 +18,17 @@ class searchPageTableViewCell: UITableViewCell {
     let ExhibitionTitleLabel = UILabel()
     let museumLabel = UILabel()
     var popularCellImageView = UIImageView()
+    let textCache = NSCache<NSString, NSString>()
+
 
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         setupViews()
+
+        selectionStyle = .none  // 셀 선택 시 하이라이트 없음
+
     }
 
     func loadImage(documentId: String) {
@@ -54,8 +59,18 @@ class searchPageTableViewCell: UITableViewCell {
     }
 
     func bindText(documentId: String) {
-        let exhibitionDetailsRef = Firestore.firestore().collection("전시_상세").document(documentId)
+        if let cachedTitle = textCache.object(forKey: "\(documentId)-title" as NSString),
+           let cachedMuseum = textCache.object(forKey: "\(documentId)-museum" as NSString) {
+            // 캐시에서 데이터를 불러옵니다.
+            DispatchQueue.main.async {
+                self.ExhibitionTitleLabel.text = cachedTitle as String
+                self.museumLabel.text = cachedMuseum as String
+            }
+            return
+        }
 
+        // Firestore에서 데이터를 가져오고 캐시에 저장
+        let exhibitionDetailsRef = Firestore.firestore().collection("전시_상세").document(documentId)
         exhibitionDetailsRef.getDocument { [weak self] (document, error) in
             guard let self = self, let document = document, document.exists else {
                 print("Document does not exist: \(error?.localizedDescription ?? "Unknown error")")
@@ -65,12 +80,17 @@ class searchPageTableViewCell: UITableViewCell {
             let exhibitionTitle = data?["전시_타이틀"] as? String ?? "Unknown Title"
             let museumName = data?["미술관_이름"] as? String ?? "Unknown Museum"
 
+            // 캐시에 저장
+            textCache.setObject(exhibitionTitle as NSString, forKey: "\(documentId)-title" as NSString)
+            textCache.setObject(museumName as NSString, forKey: "\(documentId)-museum" as NSString)
+
             DispatchQueue.main.async {
                 self.ExhibitionTitleLabel.text = exhibitionTitle
                 self.museumLabel.text = museumName
             }
         }
     }
+
 
 
     required init?(coder: NSCoder) {
@@ -81,12 +101,17 @@ class searchPageTableViewCell: UITableViewCell {
         self.backgroundColor = .black
 
         popularCellImageView.backgroundColor = .gray // 임시 배경색 설정
+        popularCellImageView.layer.cornerRadius = 15 // 모서리 둥글게
+           popularCellImageView.clipsToBounds = true    // 이미지가 뷰의 경계를 넘어가지 않도록
         contentView.addSubview(popularCellImageView)
 
         ExhibitionTitleLabel.textColor = .white
+        ExhibitionTitleLabel.font = UIFont(name: "Pretendard-Regular", size: 20)
+        ExhibitionTitleLabel.numberOfLines = 0
         contentView.addSubview(ExhibitionTitleLabel)
 
         museumLabel.textColor = .white
+        museumLabel.font = UIFont(name: "Pretendard-Regular", size: 15)
         contentView.addSubview(museumLabel)
 
 
@@ -99,13 +124,15 @@ class searchPageTableViewCell: UITableViewCell {
         }
 
         ExhibitionTitleLabel.snp.makeConstraints { make in
-            make.top.equalTo(popularCellImageView.snp.top).offset(50)
+            make.top.equalTo(popularCellImageView.snp.top).offset(20)
             make.leading.equalTo(popularCellImageView.snp.trailing).offset(10)
+            make.trailing.equalToSuperview().inset(20)
         }
 
         museumLabel.snp.makeConstraints { make in
-            make.top.equalTo(ExhibitionTitleLabel.snp.bottom).offset(5)
+            make.top.equalTo(ExhibitionTitleLabel.snp.bottom).offset(10)
             make.leading.equalTo(ExhibitionTitleLabel)
+            make.trailing.equalToSuperview().inset(20)
         }
     }
 
