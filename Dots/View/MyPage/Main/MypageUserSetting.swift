218b9d6154617c9202763e8912478887e3d894a9
judas <churoo0418@gmail.com>
2023-12-15 19:41:42 +0900

    [마이페이지_브랜치] 유저 설정 - 기능 담은 특정 셀 기능 추가

--- Dots/View/MyPage/Main/MypageUserSetting.swift
+++ Dots/View/MyPage/Main/MypageUserSetting.swift
@@ -517,14 +517,14 @@ class 마이페이지_설정_페이지 : UIViewController {
     } ()
     
     private let 설정_테이블뷰 = {
-       let tableView = UITableView()
+        let tableView = UITableView()
         tableView.backgroundColor = .clear
         tableView.layer.cornerRadius = 10
         tableView.showsVerticalScrollIndicator = false
         return tableView
     }()
     private var 설정아이템들: [[설정아이템]] = []
-
+    
     
     override func viewDidLoad() {
         view.backgroundColor = .black
@@ -539,7 +539,66 @@ class 마이페이지_설정_페이지 : UIViewController {
         
     }
     
+    func 로그아웃_유저로그아웃() {
+        do {
+            if let 현재접속중인유저 = Auth.auth().currentUser {
+                print("로그아웃한 사용자 정보:")
+                print("UID: \(현재접속중인유저.uid)")
+                print("이메일: \(현재접속중인유저.email ?? "없음")")
+                
+                let 파이어스토어 = Firestore.firestore()
+                let 이메일 = 현재접속중인유저.email ?? ""
+                let 유저컬렉션: CollectionReference
+                
+                if let providerID = 현재접속중인유저.providerData.first?.providerID, providerID == GoogleAuthProviderID {
+                    유저컬렉션 = 파이어스토어.collection("유저_데이터_관리")
+                } else {
+                    유저컬렉션 = 파이어스토어.collection("유저_데이터_관리")
+                }
+                
+                유저컬렉션.whereField("이메일", isEqualTo: 이메일).getDocuments { [weak self] (querySnapshot, 에러) in
+                    guard let self = self else { return }
+                    
+                    if let 에러 = 에러 {
+                        print("Firestore 조회 에러: \(에러.localizedDescription)")
+                    } else {
+                        if let 문서조회 = querySnapshot?.documents, !문서조회.isEmpty {
+                            let 문서 = 문서조회[0]
+                            let 현재날짜시간 = Timestamp(date: Date())
+                            
+                            문서.reference.updateData(["로그인상태": false, "마지막로그아웃": 현재날짜시간]) { (에러) in
+                                if let 에러 = 에러 {
+                                    print("Firestore 업데이트 에러: \(에러.localizedDescription)")
+                                } else {
+                                    print("Firestore: 로그인 상태 : false")
+                                    self.로그아웃_프로세스_완료()
+                                }
+                            }
+                        } else {
+                            print("Firestore: 일치하는 이메일이 없습니다.")
+                            self.로그아웃_프로세스_완료()
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("로그아웃 실패: \(error.localizedDescription)")
+        }
+    }
     
+    func 로그아웃_프로세스_완료() {
+        do {
+            try Auth.auth().signOut()
+            print("계정이 로그아웃되었습니다.")
+            let 로그인_뷰컨트롤러 = 로그인_뷰컨트롤러()
+            let 로그인화면_이동 = UINavigationController(rootViewController: 로그인_뷰컨트롤러)
+            로그인화면_이동.modalPresentationStyle = .fullScreen
+            present(로그인화면_이동, animated: true, completion: nil)
+            UserDefaults.standard.removeObject(forKey: "isUserLoggedIn")
+        } catch {
+            print("로그아웃 실패: \(error.localizedDescription)")
+        }
+    }
 }
 extension 마이페이지_설정_페이지  {
     
@@ -572,6 +631,20 @@ extension 마이페이지_설정_페이지  {
                 let 알림설정_화면이동 = 알림설정_화면()
                 navigationController?.pushViewController(알림설정_화면이동, animated: true)
             }
+            else if 셀_제목_라벨 == "로그아웃" {
+                let 로그아웃_알럿 = UIAlertController(title: "로그아웃", message: "지금 사용중인 기기에서 로그아웃을 진행할까요?", preferredStyle: .alert)
+                
+                let 로그아웃취소_버튼 = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+                로그아웃_알럿.addAction(로그아웃취소_버튼)
+                
+                let 로그아웃확인_버튼 = UIAlertAction(title: "로그아웃", style: .destructive) { _ in
+                    self.로그아웃_유저로그아웃()
+                }
+                로그아웃_알럿.addAction(로그아웃확인_버튼)
+                
+                present(로그아웃_알럿, animated: true, completion: nil)
+            }
+            
         }
     }
 }
@@ -583,7 +656,7 @@ extension 마이페이지_설정_페이지 {
         view.addSubview(뒤로가기_버튼)
         view.addSubview(페이지_제목)
         view.addSubview(설정_테이블뷰)
-
+        
         뒤로가기_버튼.snp.makeConstraints { make in
             make.centerY.equalTo(페이지_제목.snp.centerY)
             make.leading.equalToSuperview().offset(16)
@@ -615,19 +688,19 @@ extension 마이페이지_설정_페이지 {
             [설정아이템(title: "회원 탈퇴", isSwitchOn: false, action: { /* 회원 탈퇴 기능 구현 */ })]
         ]
     }
-   }
+}
 
 
 
 extension 마이페이지_설정_페이지 : UITableViewDelegate, UITableViewDataSource {
-     func numberOfSections(in tableView: UITableView) -> Int {
-           return 설정아이템들.count
-       }
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return 설정아이템들[section].count
-       }
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 설정아이템들.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 설정아이템들[section].count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let 셀 = tableView.dequeueReusableCell(withIdentifier: "설정_셀", for: indexPath) as! 설정_셀
         let 설정아이템 = 설정아이템들[indexPath.section][indexPath.row]
@@ -641,58 +714,67 @@ extension 마이페이지_설정_페이지 : UITableViewDelegate, UITableViewDat
             스위치.addTarget(self, action: #selector(프로필스위치변경), for: .valueChanged)
             셀.accessoryView = 스위치
             셀.selectionStyle = .none
+            셀.layer.cornerRadius = 20
+            
         } else if indexPath.section == 1 {
             셀.accessoryType = .disclosureIndicator
             셀.tintColor = UIColor.lightGray
+            if indexPath.row == 0 {
+                셀.layer.cornerRadius = 20
+                셀.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            } else if indexPath.row == 3 {
+                셀.layer.cornerRadius = 20
+                셀.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            }
+        }  else if indexPath.section == 2 && indexPath.row == 0{
+            셀.layer.cornerRadius = 20
             
-        } else {
+        }
+        else {
             셀.accessoryType = .none
         }
         if indexPath.section == 3 && indexPath.row == 0 {
             셀.버튼_이름_라벨.textColor = .red
-           } else if indexPath.section == 1 {
-               셀.accessoryType = .disclosureIndicator
-               셀.tintColor = UIColor.lightGray
-           } else {
-               셀.accessoryType = .none
-           }
+            셀.selectionStyle = .none
+            셀.layer.cornerRadius = 20
+        }
         return 셀
     }
-
-
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           tableView.deselectRow(at: indexPath, animated: true)
-//           설정아이템들[indexPath.section][indexPath.row].action()
-            if indexPath.section == 1 {
-                let selectedCell = (tableView.cellForRow(at: indexPath) as? 설정_셀)!
-                   셀_클릭(for: selectedCell)
-               }
-       }
     
-
-        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-           switch section {
-           case 0:
-               return " "
-           case 1:
-               return " "
-           case 2:
-               return " "
-           case 3:
-               return " "
-           default:
-               return nil
-           }
-       }
-   
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //           설정아이템들[indexPath.section][indexPath.row].action()
+        
+        let selectedCell = (tableView.cellForRow(at: indexPath) as? 설정_셀)!
+        셀_클릭(for: selectedCell)
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return " "
+        case 1:
+            return " "
+        case 2:
+            return " "
+        case 3:
+            return " "
+        default:
+            return nil
+        }
+    }
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 0.0
     }
-   }
+}
 
 class 설정_셀: UITableViewCell {
     
-     let 버튼_이름_라벨 = {
+    let 버튼_이름_라벨 = {
         let label = UILabel()
         label.textColor = .white
         return label
