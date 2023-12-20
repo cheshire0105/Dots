cf59ca2eeba446dec51eef70fba36b061ea930c8
judas <churoo0418@gmail.com>
2023-12-21 05:20:50 +0900

    [마이페이지_브랜치] 캘런더 - sheetpresent - 테이블뷰 데이터 가져오기 / 이미지 기존값 증식 되는 부분 작업중

--- Dots/View/MyPage/Calendar/CalendarSchedule.swift
+++ Dots/View/MyPage/Calendar/CalendarSchedule.swift
@@ -4,8 +4,8 @@ import FirebaseFirestore
 import FirebaseStorage
 class 캘린더_스케쥴_등록_모달 : UIViewController {
     
-   
-
+    
+    
     
     private let 손잡이 = {
         let uiView = UIView()
@@ -47,9 +47,15 @@ class 캘린더_스케쥴_등록_모달 : UIViewController {
         캘린더_전시_테이블뷰.dataSource = self
         캘린더_전시_테이블뷰.register(캘린더_스케쥴_등록_셀.self, forCellReuseIdentifier: "캘린더_스케쥴_등록_셀")
         
+        
+        데이터가져오기및UI업데이트()
+        
+    }
+    deinit {
+        // 뷰 컨트롤러가 해제되면 리스너도 제거
+        listener?.remove()
     }
 }
-
 
 
 
@@ -66,24 +72,95 @@ struct 셀_데이터 {
 }
 
 struct 셀_배열 {
-    var 셀_데이터_배열: [셀_데이터] = [
-        셀_데이터(포스터이미지: "이미지1", 전시명: "전시1", 장소: "장소1", 방문날짜: "날짜1"),
-        셀_데이터(포스터이미지: "이미지2", 전시명: "전시2", 장소: "장소2", 방문날짜: "날짜2"),
-        셀_데이터(포스터이미지: "이미지3", 전시명: "전시3", 장소: "장소3", 방문날짜: "날짜3"),
-        셀_데이터(포스터이미지: "이미지4", 전시명: "전시4", 장소: "장소4", 방문날짜: "날짜4"),
-    ]
+    var 셀_데이터_배열: [셀_데이터]
+    
+    init() {
+        self.셀_데이터_배열 = [
+          
+        ]
+    }
 }
 
 var 셀_데이터_배열 = 셀_배열()
 
+var listener: ListenerRegistration?
+
+
+extension 캘린더_스케쥴_등록_모달 {
+    
+    func 데이터가져오기및UI업데이트() {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            print("유저가 접속 상태가 아님")
+            return
+        }
+        
+        let 데이터베이스 = Firestore.firestore()
+        
+        데이터베이스.collection("posters").getDocuments { (쿼리스냅샷, 에러) in
+            if let 에러 = 에러 {
+                print("포스터스 문서 가져오기 에러: \(에러.localizedDescription)")
+                return
+            }
+            
+            guard let 포스터스문서들 = 쿼리스냅샷?.documents else {
+                print("포스터스 문서가 없습니다.")
+                return
+            }
+            
+            for 포스터스문서 in 포스터스문서들 {
+                let 포스터스문서ID = 포스터스문서.documentID
+                
+                데이터베이스.collection("posters").document(포스터스문서ID).collection("reviews").document(uid).getDocument { (리뷰문서스냅샷, 리뷰에러) in
+                    if let 리뷰에러 = 리뷰에러 {
+                        print("리뷰 문서 가져오기 에러: \(리뷰에러.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let 리뷰문서데이터 = 리뷰문서스냅샷?.data(),
+                          let 포스터이미지 = 리뷰문서데이터["포스터이미지"] as? String,
+                          let 방문날짜 = 리뷰문서데이터["유저_다녀옴_날짜"] as? String else {
+                        return
+                    }
+                    
+                    데이터베이스.collection("전시_상세").document(포스터스문서ID).getDocument { (전시상세스냅샷, 전시에러) in
+                        if let 전시에러 = 전시에러 {
+                            print("전시 상세 가져오기 에러: \(전시에러.localizedDescription)")
+                            return
+                        }
+                        
+                        guard let 전시상세데이터 = 전시상세스냅샷?.data(),
+                              let 전시명 = 전시상세데이터["전시_타이틀"] as? String,
+                              let 전시장소 = 전시상세데이터["미술관_이름"] as? String else {
+                            return
+                        }
+                        
+                        let 데이터 = 셀_데이터(포스터이미지: 포스터이미지, 전시명: 전시명, 장소: 전시장소, 방문날짜: 방문날짜)
+                        셀_데이터_배열.셀_데이터_배열.append(데이터)
+                        self.캘린더_전시_테이블뷰.reloadData()
+                        print(셀_데이터_배열.셀_데이터_배열)
+
+                    }
+                }
+            }
+        }
+    }
+
+}
+//
+//
+//
+//
+//
+//
+//
 
 
 extension 캘린더_스케쥴_등록_모달: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-  1
+        1
     }
-
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 셀_데이터_배열.셀_데이터_배열.count
@@ -95,13 +172,15 @@ extension 캘린더_스케쥴_등록_모달: UITableViewDelegate, UITableViewDat
         }
         
         let 데이터 = 셀_데이터_배열.셀_데이터_배열[indexPath.row]
-        cell.셀_데이터 = 데이터
-
+        cell.전시_포스터_이미지.image = UIImage(named: 데이터.포스터이미지)
+        cell.전시명_라벨.text = 데이터.전시명
+        cell.장소_라벨.text = 데이터.장소
+        cell.방문날짜_라벨.text = 데이터.방문날짜
         if cell.isSelected {
-             cell.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1)
-         } else {
-             cell.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1)
-         }
+            cell.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1)
+        } else {
+            cell.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1)
+        }
         return cell
     }
     
@@ -110,8 +189,8 @@ extension 캘린더_스케쥴_등록_모달: UITableViewDelegate, UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                tableView.cellForRow(at: indexPath)?.selectionStyle = .none
-                print("셀클릭")
+        tableView.cellForRow(at: indexPath)?.selectionStyle = .none
+        print("셀클릭")
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -131,12 +210,12 @@ extension 캘린더_스케쥴_등록_모달: UITableViewDelegate, UITableViewDat
             
             if let 현제모달 = self.findViewController() {
                 현제모달.dismiss(animated: true) {
-                         let 새모달 = 켈린더_수정_뷰컨트롤러()
+                    let 새모달 = 켈린더_수정_뷰컨트롤러()
                     새모달.modalPresentationStyle = .fullScreen
                     현제모달.present(새모달, animated: false, completion: nil)
-                     }
-                 }
-
+                }
+            }
+            
             completionHandler(true)
         }
         수정.image = UIImage(named: "내후기edit")
@@ -148,12 +227,12 @@ extension 캘린더_스케쥴_등록_모달: UITableViewDelegate, UITableViewDat
             
             if let 현제모달 = self.findViewController() {
                 현제모달.dismiss(animated: true) {
-                         let 새모달 = 켈린더_삭제_뷰컨트롤러()
+                    let 새모달 = 켈린더_삭제_뷰컨트롤러()
                     새모달.modalPresentationStyle = .fullScreen
                     현제모달.present(새모달, animated: false, completion: nil)
                     
-                     }
-                 }
+                }
+            }
             completionHandler(true)
         }
         삭제.image = UIImage(named: "내후기delete")
@@ -162,8 +241,8 @@ extension 캘린더_스케쥴_등록_모달: UITableViewDelegate, UITableViewDat
         
         let configuration = UISwipeActionsConfiguration(actions: [삭제, 수정])
         수정.accessibilityValue = "수정 버튼"
-            삭제.accessibilityValue = "삭제 버튼"
-            configuration.performsFirstActionWithFullSwipe = false
+        삭제.accessibilityValue = "삭제 버튼"
+        configuration.performsFirstActionWithFullSwipe = false
         return configuration
     }
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
