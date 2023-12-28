3972e22a77c51a8b5b034bbabb8aa62bfadb225c
judas <churoo0418@gmail.com>
2023-12-28 13:30:42 +0900

    [마이페이지_브랜치]  캘린더- 테이블뷰 셀 삭제 대상 데이터 전달확인 업데이트

--- Dots/View/MyPage/Calendar/CalendarDelete.swift
+++ Dots/View/MyPage/Calendar/CalendarDelete.swift
@@ -69,6 +69,17 @@ class 켈린더_삭제_뷰컨트롤러 : UIViewController {
               배경_백.addGestureRecognizer(panGestureRecognizer)
         let panGestureRecognizer2 = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
               삭제_뷰.addGestureRecognizer(panGestureRecognizer2)
+        
+        
+        print("다녀온 날짜 삭제/유지 화면")
+        print("----전달 받은 셀 데이터----")
+        print("포스터이미지URL: \(삭제할셀데이터?.포스터이미지URL ?? "")")
+        print("전시명: \(삭제할셀데이터?.전시명 ?? "")")
+        print("장소: \(삭제할셀데이터?.장소 ?? "")")
+        print("방문날짜: \(삭제할셀데이터?.방문날짜 ?? "")")
+        print("리뷰문서ID: \(삭제할셀데이터?.리뷰문서ID ?? "")")
+        print("포스터스문서ID: \(삭제할셀데이터?.포스터스문서ID ?? "")")
+        
     }
     @objc private func handleBackgroundTap() {
             dismiss(animated: false, completion: nil)
@@ -95,6 +106,15 @@ extension 켈린더_삭제_뷰컨트롤러 {
     }
     @objc func 삭제하기_버튼_클릭 () {
         print("다녀온 일정을 삭제했습니다.")
+        
+        
+        
+        
+        
+        
+        
+        
+        
         dismiss(animated: false, completion: nil)
 
 
